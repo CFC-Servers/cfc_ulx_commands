2ec2979a2e7ed4092b345b49e2bf7db199eb030d
= <damierny@gmail.com>
2019-07-19 10:49:19 -0500

    Fix HMM's suggestions, reduced it to 1 command and did a lookup table instead of table.HasValue

--- lua/ulx/modules/sh/freeze.lua
+++ lua/ulx/modules/sh/freeze.lua
@@ -1,50 +1,28 @@
 CATEGORY_NAME = "CFC"
 
-do
-    local function freezeProps( callingPlayer, targetPlayers ) 
-        local entits = ents.FindByClass( "prop_*" )
-        local entCount = 0
-        
-        for _, ent in pairs( entits ) do
-            if table.HasValue( targetPlayers, ent:CPPIGetOwner() ) then 
-                local physicsObj = ent:GetPhysicsObject()
-                if IsValid( physicsObj ) then
-                    physicsObj:EnableMotion( false )
-                    physicsObj:Sleep()
-                    entCount = entCount + 1
-                end
-            end
-        end
-
-        ulx.fancyLogAdmin( callingPlayer, "#A froze all "..entCount.." props owned by #T", targetPlayers )
+local function freezeProps( callingPlayer, targetPlayers ) 
+    local entities = ents.FindByClass( "prop_*" )
+    local entCount = 0
+    local isTarget = {}
+    for _, ply in pairs( targetPlayers ) do
+        isTarget[ply] = true
     end
 
-    local entits = ulx.command( CATEGORY_NAME, "ulx freezeprops", freezeProps, "!freezeprops" )
-    entits:addParam{ type=ULib.cmds.PlayersArg }
-    entits:defaultAccess( ULib.ACCESS_ADMIN )
-    entits:help( "Freezes target(s) props" )
-end
-
-do
-    local function freezeSelfProps( callingPlayer ) 
-        local entits = ents.FindByClass( "prop_*" )
-        local entCount = 0
-        
-        for _, ent in pairs( entits ) do
-            if ent:CPPIGetOwner() == callingPlayer then
-                local physicsObj = ent:GetPhysicsObject()
-                if IsValid( physicsObj ) then
-                    physicsObj:EnableMotion( false )
-                    physicsObj:Sleep()
-                    entCount = entCount + 1
-                end
+    for _, ent in pairs( entities ) do
+        if isTarget[ent:CPPIGetOwner()] then
+            local physicsObj = ent:GetPhysicsObject()
+            if IsValid( physicsObj ) then
+                physicsObj:EnableMotion( false )
+                physicsObj:Sleep()
+                entCount = entCount + 1
             end
         end
-
-        ulx.fancyLogAdmin( callingPlayer, "#A froze all "..entCount.." of their props." )
     end
 
-    local entits = ulx.command( CATEGORY_NAME, "ulx freezeself", freezeSelfProps, "!freezeself" )
-    entits:defaultAccess( ULib.ACCESS_ADMIN )
-    entits:help( "Freezes Caller's props" )
+    ulx.fancyLogAdmin( callingPlayer, "#A froze all "..entCount.." props owned by #T", targetPlayers )
 end
+
+local entities = ulx.command( CATEGORY_NAME, "ulx freezeprops", freezeProps, "!freezeprops" )
+entities:addParam{ type=ULib.cmds.PlayersArg }
+entities:defaultAccess( ULib.ACCESS_ADMIN )
+entities:help( "Freezes target(s) props" )
