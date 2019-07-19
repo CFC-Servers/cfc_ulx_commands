c56b78e440718b2a85d6a1f969cf1662d7e867ff
= <damierny@gmail.com>
2019-07-19 13:57:58 -0500

    Added prop counter, and increased the types of entities it can freeze.

--- lua/ulx/modules/sh/freeze.lua
+++ lua/ulx/modules/sh/freeze.lua
@@ -1,25 +1,32 @@
 CATEGORY_NAME = "CFC"
 
 local function freezeProps( callingPlayer, targetPlayers ) 
-    local entities = ents.FindByClass( "prop_*" )
+    local entities = ents.GetAll()
     local entCount = 0
-    local isTarget = {}
+    local entCounts = {}
     for _, ply in pairs( targetPlayers ) do
-        isTarget[ply] = true
+        entCounts[ply] = 0
     end
 
     for _, ent in pairs( entities ) do
-        if isTarget[ent:CPPIGetOwner()] then
+        local owner = ent:CPPIGetOwner()
+        if entCounts[owner] then
+            local canFreeze = not (ent:IsWeapon() or ent:GetUnFreezable() or ent:IsPlayer())
             local physicsObj = ent:GetPhysicsObject()
-            if IsValid( physicsObj ) then
+            if IsValid( physicsObj ) and canFreeze then
                 physicsObj:EnableMotion( false )
                 physicsObj:Sleep()
                 entCount = entCount + 1
+                entCounts[owner] = entCounts[owner] + 1
             end
         end
     end
-
-    ulx.fancyLogAdmin( callingPlayer, "#A froze "..entCount.." props owned by #T", targetPlayers )
+    local concatVal = ""
+    for _, num in pairs( entCounts ) do
+        concatVal = concatVal .. num .. ", "
+    end
+    concatVal = string.sub( concatVal, 1, #concatVal - 2 )
+    ulx.fancyLogAdmin( callingPlayer, "#A froze "..concatVal.." props owned by #T, respectively. " .. entCount .. " props in total.", targetPlayers )
 end
 
 local freezeCMD = ulx.command( CATEGORY_NAME, "ulx freezeprops", freezeProps, "!freezeprops" )
