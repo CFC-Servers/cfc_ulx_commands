5420a0049bab4f1bf7c2375691a49a1285dd9b75
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-11-14 21:25:08 -0500

    Update param order, split into separate files

--- lua/ulx/modules/sh/cfc_cleanup.lua
+++ lua/ulx/modules/sh/cfc_cleanup.lua
@@ -1,40 +1,10 @@
 CATEGORY_NAME = "Cleanup"
 
-local function ropeClean( callingPlayer, targetPlayers )
-    local plyCounts = {}
-    for _, ply in pairs( targetPlayers ) do
-        plyCounts[ply] = 0
+local function cleanupPlayerEnts( callingPlayer, targetPlayers, targetEntities )
+    if targetEntities == nil then
+        targetEntities = "*"
     end
 
-    local ropes = ents.FindByClass( "keyframe_rope" )
-    local ropeCount = 0
-
-    for _, rope in pairs( ropes ) do
-        local owner = rope:CPPIGetOwner()
-
-        if plyCounts[owner] ~= nil then
-            rope:Remove()
-            ropeCount = ropeCount + 1
-            plyCounts[owner] = plyCounts[owner] + 1
-        end
-    end
-
-    ulx.fancyLogAdmin( callingPlayer, "#A removed " .. ropeCount .. " ropes owned by #T", targetPlayers )
-
-    for ply, count in pairs( plyCounts ) do
-        if count > 0 then
-            callingPlayer:ChatPrint( string.format( "%s owned %u ropes", ply:GetName(), count ) )
-        end
-    end
-end
-
-local ropes = ulx.command( CATEGORY_NAME, "ulx ropeclean", ropeClean, "!ropeclean" )
-ropes:addParam{ type = ULib.cmds.PlayersArg }
-ropes:defaultAccess( ULib.ACCESS_ADMIN )
-ropes:help( "Remove target(s) ropes" )
-
-
-local function cleanupPlayerEnts( callingPlayer, targetEntities, targetPlayers )
     local isTarget = {}
     for _, ply in pairs( targetPlayers ) do
         isTarget[ply] = true
@@ -44,11 +14,12 @@ local function cleanupPlayerEnts( callingPlayer, targetEntities, targetPlayers )
 
     local isWildcardMatch = targetEntities == "*"
 
-    for _, ent in ipairs( ents.GetAll() ) do
+    for ent, _ in ipairs( ents.GetAll() ) do
         if not ent:IsWeapon() then
             local isModelMatch = targetEntities == ent:GetModel()
             local isClassMatch = targetEntities == ent:GetClass()
             local isMatch = isWildcardMatch or isModelMatch or isClassMatch
+
             if isMatch then
                 local owner = ent.CPPIGetOwner and ent:CPPIGetOwner()
                 if isTarget[owner] then
@@ -60,11 +31,10 @@ local function cleanupPlayerEnts( callingPlayer, targetEntities, targetPlayers )
     end
 
     ulx.fancyLogAdmin( callingPlayer,  "#A removed " .. count .. " entities owned by #T", targetPlayers )
-
 end
 
 local cleanup = ulx.command( CATEGORY_NAME, "ulx cleanup", cleanupPlayerEnts, "!cleanup" )
-cleanup:addParam{ type = ULib.cmds.StringArg, hint="class/model, * for all" }
 cleanup:addParam{ type = ULib.cmds.PlayersArg }
+cleanup:addParam{ type = ULib.cmds.StringArg, hint="class/model, * for all" }
 cleanup:defaultAccess( ULib.ACCESS_ADMIN )
-cleanup:help( "Remove targets entities" )
+cleanup:help( "Remove entities owned by target" )
---
+++ lua/ulx/modules/sh/cfc_ropeclean.lua
@@ -0,0 +1,36 @@
+CATEGORY_NAME = "Cleanup"
+
+local function ropeClean( callingPlayer, targetPlayers )
+    local plyCounts = {}
+    for _, ply in pairs( targetPlayers ) do
+        plyCounts[ply] = 0
+    end
+
+    local ropes = ents.FindByClass( "keyframe_rope" )
+    local ropeCount = 0
+
+    for _, rope in pairs( ropes ) do
+        local owner = rope:CPPIGetOwner()
+
+        if plyCounts[owner] ~= nil then
+            rope:Remove()
+            ropeCount = ropeCount + 1
+            plyCounts[owner] = plyCounts[owner] + 1
+        end
+    end
+
+    ulx.fancyLogAdmin( callingPlayer, "#A removed " .. ropeCount .. " ropes owned by #T", targetPlayers )
+
+    for ply, count in pairs( plyCounts ) do
+        if count > 0 then
+            callingPlayer:ChatPrint( string.format( "%s owned %u ropes", ply:GetName(), count ) )
+        end
+    end
+end
+
+local ropes = ulx.command( CATEGORY_NAME, "ulx ropeclean", ropeClean, "!ropeclean" )
+ropes:addParam{ type = ULib.cmds.PlayersArg }
+ropes:defaultAccess( ULib.ACCESS_ADMIN )
+ropes:help( "Remove target(s) ropes" )
+
+
