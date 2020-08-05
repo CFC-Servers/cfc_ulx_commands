c976a7b68ad86879bfe90510d83491ce89058421
HarryCallahan308 <jringel2@gmail.com>
2020-08-05 19:26:34 -0400

    Give acknowledgement and rework entire command
    Credit goes to BullyHunter for outside help.

--- lua/ulx/modules/sh/cfc_refund.lua
+++ lua/ulx/modules/sh/cfc_refund.lua
@@ -1,37 +1,40 @@
+--[[Credit goes to Phatso for the idea and BullyHunter for the extra support.]]
+
 -- This resets kills and deaths caused by the cheater.
 local CATEGORY_NAME = "Refund"
 
 -- Storing table for kills on a player from a cheater.
-local playerKills = {}
+local kills = {}
 
--- Initialize the table when the cheater spawns.
-hook.Add( "PlayerInitialSpawn", "CFC_ULXCommands_CreateDeathTable", function(ply)
-	playerKills[ply] = {}
-end)
-
--- Still keep track of it after disconnect. Forgot to add do.
-hook.Add( "PlayerDisconnected", "CFC_ULXCommands_KeepingTrackofDeathTable", function(ply)
-	playerKills[ply] = nil
-
-	for _, killData in pairs( playerKills ) do
-		killData[ply] = nil
-	end
-end)
+for k, v in ipairs( player.GetAll() ) do
+	kills[ v ] = {}
+end
 
 -- Storing ply and attacker in playerKills table and reversing the deaths inflicted by the cheater.
 hook.Add( "PlayerDeath", "CFC_ULXCommands_PlayerDeath", function( ply, inflictor, attacker )
-	local playerKillsOnVictim = playerKills[attacker][ply] or 0
-	playerKills[attacker][ply] = playerKillsOnVictim + 1
+	local x = kills[ ply ][ attacker ] or 0
+	kills[ply][attacker] = kills[ ply ][ attacker] + 1
+end)
+	
+function HackerMan( ply )
+	for k, v in pairs( kills ) do
+		for x, p in pairs(v) do
+			if ( x == ply ) then
+				killsp[ k ][ x ] = nil
+			end
+		end
+	end
+end
+
+-- Still keep track of it after disconnect. Forgot to add do.
+hook.Add( "PlayerDisconnected", "CFC_ULXCommands_KeepingTrackofDeathTable", function(ply)
+	HackerMan(ply)
 end)
 
 -- Remove frags caused by the cheater.
 local function playerRefunds( callingPlayer, targetPlayers )
 
-	for k, v in pairs( playerKills ) do
-		if not IsValid(v) then break end
-		local victimDeaths = victim:Deaths()
-		victim:SetDeaths( victimDeaths - v )
-	end
+	return HackerMan() end
 
 	ulx.fancyLogAdmin( callingPlayer, "#A has reset #T 's deaths and kills", targetPlayers)
 
