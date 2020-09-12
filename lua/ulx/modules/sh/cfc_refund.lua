f9418d4c120f9027b7c0310d1c3034ac31f75bfc
HarryCallahan308 <jringel2@gmail.com>
2020-09-12 16:53:25 -0400

    Better naming conventions

--- lua/ulx/modules/sh/cfc_refund.lua
+++ lua/ulx/modules/sh/cfc_refund.lua
@@ -4,42 +4,41 @@
 local CATEGORY_NAME = "Refund"
 
 -- Storing table for kills on a player from a cheater.
-local kills = {}
+local deaths = {}
 
 hook.Add( "PlayerInitialSpawn", "CFC_ULXCommands_InitialSpawn", function(ply)
-	kills[ply] = {}
+	deaths[ply] = {}
 end)
 
 -- Storing ply and attacker in playerKills table and reversing the deaths inflicted by the cheater.
+-- Storing ply and attacker in playerKills table and reversing the deaths inflicted by the cheater.
 hook.Add( "PlayerDeath", "CFC_ULXCommands_PlayerDeath", function( ply, inflictor, attacker )
-	if not IsValid(attacker) then return end
-	if ply == attacker then return end
-	local x = kills[ ply ][ attacker ] or 0
-	kills[ ply ][ attacker ] = x + 1
+    if not IsValid( attacker ) then return end
+    if ply == attacker then return end
+
+    local deathCount = deaths[ply][attacker] or 0
+    deaths[ply][attacker] = deathCount + 1
 end)
 
-function HackerMan( ply )
-
-	for k, v in pairs( kills ) do
-		for x, p in pairs(v) do
-			if ( x == ply ) then
-				kills[ k ][ x ] = nil
-			end
-		end
-	end
-
+function removeKills( ply )
+    for victim, plyDeaths in pairs( deaths ) do
+        for attacker in pairs( plyDeaths ) do
+            if attacker == ply then
+                deaths[victim][attacker] = nil
+            end
+        end
+    end
 end
-
 -- Still keep track of it after disconnect. Forgot to add do.
 hook.Add( "PlayerDisconnected", "CFC_ULXCommands_KeepingTrackofDeathTable", function(ply)
-	HackerMan(ply)
+	removeKills(ply)
 end)
 
 -- Remove frags caused by the cheater.
 local function playerRefunds( callingPlayer, targetPlayers )
 
 	for _, ply in pairs( targetPlayers ) do
-		HackerMan(ply)
+		removeKills(ply)
 	end
 
 	ulx.fancyLogAdmin( callingPlayer, "#A has reset #T 's deaths and kills", targetPlayers)
