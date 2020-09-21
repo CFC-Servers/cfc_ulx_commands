e6b9d1b84a957ef00f992d7b807fe21d3f3775c3
PLally <11771225+PLally@users.noreply.github.com>
2020-09-21 01:09:59 -0400

    properly set targeTentities default

--- lua/ulx/modules/sh/cfc_cleanup.lua
+++ lua/ulx/modules/sh/cfc_cleanup.lua
@@ -1,7 +1,7 @@
 CATEGORY_NAME = "Cleanup"
 
 local function cleanupPlayerEnts( callingPlayer, targetPlayers, targetEntities )
-    targetEntities = targetEntities or "*"
+    targetEntities = targetEntities
 
     local isTarget = {}
     for _, ply in pairs( targetPlayers ) do
@@ -33,6 +33,6 @@ end
 
 local cleanup = ulx.command( CATEGORY_NAME, "ulx cleanup", cleanupPlayerEnts, "!cleanup" )
 cleanup:addParam{ type = ULib.cmds.PlayersArg }
-cleanup:addParam{ type = ULib.cmds.StringArg, hint = "class/model, * for all", ULib.cmds.optional }
+cleanup:addParam{ type = ULib.cmds.StringArg, hint = "class/model, * for all", ULib.cmds.optional, default="*" }
 cleanup:defaultAccess( ULib.ACCESS_ADMIN )
 cleanup:help( "Remove entities owned by target" )
