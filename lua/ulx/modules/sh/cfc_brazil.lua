c94a5ff4177eabb5bdf0ce2f97b301751f53ce14
legokidlogan <legokidlogan@gmail.com>
2020-10-28 09:46:24 -0600

    Conform to guidelines

--- lua/ulx/modules/sh/cfc_brazil.lua
+++ lua/ulx/modules/sh/cfc_brazil.lua
@@ -2,55 +2,55 @@ local MIN_X, MIN_Y, MIN_Z = -16384, -16384, -16384 / 2
 local MAX_X, MAX_Y, MAX_Z = 16384, 16384, 16384 / 2
 local mapPositionData = {
     ["gm_bigcity"] = {
-        fallback = Vector( 9192, 10270, -10897 ),
+        fallBack = Vector( 9192, 10270, -10897 ),
         offsetX = 100,
         offsetY = 200
     },
     ["gm_bigcity_improved"] = {
-        fallback = Vector( 9192, 10270, -10897 ),
+        fallBack = Vector( 9192, 10270, -10897 ),
         offsetX = 100,
         offsetY = 200
     },
     ["gm_bigcity_improved_lite"] = {
-        fallback = Vector( 9192, 10270, -10897 ),
+        fallBack = Vector( 9192, 10270, -10897 ),
         offsetX = 100,
         offsetY = 200
     },
     ["gm_construct"] = {
-        fallback = Vector( -2069, -3316, -252 ),
+        fallBack = Vector( -2069, -3316, -252 ),
         offsetX = 1000,
         offsetY = 500
     },
     ["gm_novenka"] = {
-        fallback = Vector( 9760, -13800, -700 ),
+        fallBack = Vector( 9760, -13800, -700 ),
         offsetX = 800,
         offsetY = 500
     },
     ["gm_bluehills"] = {
-        fallback = Vector( -8750, -8500, -1084 ),
+        fallBack = Vector( -8750, -8500, -1084 ),
         offsetX = 500,
         offsetY = 500
     },
     ["gm_bluehills_test3"] = {
-        fallback = Vector( -8750, -8500, -1084 ),
+        fallBack = Vector( -8750, -8500, -1084 ),
         offsetX = 500,
         offsetY = 500
     }, 
     ["gm_flatgrass"] = {
-        fallback = Vector( -700, 100, -12764 ),
+        fallBack = Vector( -700, 100, -12764 ),
         offsetX = 250,
         offsetY = 500
     }, 
     ["gm_functional_flatgrass"] = {
-        fallback = Vector( 10500, -9400, -16252 ),
+        fallBack = Vector( 10500, -9400, -16252 ),
         offsetX = 3500,
         offsetY = 3500
     }, 
     ["gm_genesis"] = {
-        fallback = Vector( 6800, 6500, -14974 ),
+        fallBack = Vector( 6800, 6500, -14974 ),
         offsetX = 600,
         offsetY = 600
-    }, 
+    }
 }
 
 local function planTrip()
