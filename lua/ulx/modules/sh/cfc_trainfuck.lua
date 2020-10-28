3fbea29b7813424372e425df6d21fdd3bb7cf47c
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-10-28 05:47:13 +0100

    Added ULX functionality and made it automatically unnoclip noclipped players.

--- lua/ulx/modules/sh/cfc_trainfuck.lua
+++ lua/ulx/modules/sh/cfc_trainfuck.lua
@@ -1,8 +1,8 @@
-local plys = player.GetAll()
+CATEGORY_NAME = "Trainfuck"
 
-function trainFuck()
+local function trainFuck( callingPlayer, targetPlayers )
 	local Y = 1
-	for I,ply in pairs(plys) do
+	for I,ply in pairs(targetPlayers) do
 		local train = ents.Create("prop_physics")
 		train:SetModel("models/props_trainstation/train001.mdl")
 		train:SetPos(ply:GetPos()+Vector(0,-1000,300))
@@ -11,14 +11,21 @@ function trainFuck()
 		local phys = train:GetPhysicsObject()
 		
 		if (IsValid(phys)) then
+			if ply:GetMoveType(MOVETYPE_NOCLIP) then
+				ply:SetMoveType(MOVETYPE_WALK)
+			end
+			ply:ExitVehicle()
 			phys:ApplyForceCenter((ply:GetPos()-train:GetPos())*1000000000)
-			
 			timer.Create("trainFuckTimer"..Y,1.5,1,function()
 				train:Remove()
 			end)
 		end
 		Y = Y + 1
 	end
+	ulx.fancyLogAdmin( callingPlayer, "#A trainfucked #T", targetPlayers )
 end
 
-trainFuck()
+local trainfuckCMD = ulx.command( CATEGORY_NAME, "ulx trainfuck", trainFuck, "!trainfuck" )
+trainfuckCMD:addParam{ type = ULib.cmds.PlayersArg }
+trainfuckCMD:defaultAccess( ULib.ACCESS_ADMIN )
+trainfuckCMD:help( "Trainfucks target( s )" )
