42ebffcdacd9ad08e60bd035b89c0d29c8e0d42c
legokidlogan <legokidlogan@gmail.com>
2020-10-28 15:07:04 -0600

    Prevent teleports to skybox

--- lua/ulx/modules/sh/cfc_brazil.lua
+++ lua/ulx/modules/sh/cfc_brazil.lua
@@ -1,54 +1,67 @@
 local CATEGORY_TYPE = "Teleport"
-local MIN_X, MIN_Y, MIN_Z = -16384, -16384, -16384 / 2
-local MAX_X, MAX_Y, MAX_Z = 16384, 16384, 16384 / 2
-local HULL_BUFFER = 5
+local MIN_X, MIN_Y, MIN_Z = -16384, -16384, -16384
+local MAX_X, MAX_Y, MAX_Z = 16384, 16384, 16384
+local HULL_BUFFER = 2
 local MAP_POSITION_DATA = {
     ["gm_bigcity"] = {
+        centerZ = 14500 / 2 - 11400,
+        height = 14500 / 2,
         fallback = Vector( 9192, 10270, -10897 ),
-        offsetX = 100,
-        offsetY = 200
+        offsetX = 900,
+        offsetY = 1900
     },
     ["gm_bigcity_improved"] = {
+        centerZ = 14500 / 2 - 11400,
+        height = 14500 / 2,
         fallback = Vector( 9192, 10270, -10897 ),
-        offsetX = 100,
-        offsetY = 200
+        offsetX = 900,
+        offsetY = 1900
     },
     ["gm_bigcity_improved_lite"] = {
+        centerZ = 14500 / 2 - 11400,
+        height = 14500 / 2,
         fallback = Vector( 9192, 10270, -10897 ),
-        offsetX = 100,
-        offsetY = 200
+        offsetX = 900,
+        offsetY = 1900
     },
     ["gm_construct"] = {
+        centerZ = 10500 / 2 - 500,
+        height = 10500 / 2,
         fallback = Vector( -2069, -3316, -252 ),
         offsetX = 1000,
         offsetY = 500
     },
     ["gm_novenka"] = {
+        centerZ = 12800 / 2 - 2050,
+        height = 12800 / 2,
         fallback = Vector( 9760, -13800, -700 ),
         offsetX = 800,
         offsetY = 500
     },
-    ["gm_bluehills"] = {
-        fallback = Vector( -8750, -8500, -1084 ),
-        offsetX = 500,
-        offsetY = 500
-    },
     ["gm_bluehills_test3"] = {
+        centerZ = 14400 / 2 - 1100,
+        height = 14400 / 2,
         fallback = Vector( -8750, -8500, -1084 ),
         offsetX = 500,
         offsetY = 500
     }, 
     ["gm_flatgrass"] = {
+        centerZ = 28200 / 2 - 12800,
+        height = 28200 / 2,
         fallback = Vector( -700, 100, -12764 ),
         offsetX = 250,
         offsetY = 500
     }, 
     ["gm_functional_flatgrass"] = {
+        centerZ = 28600 / 2 - 16300,
+        height = 28600 / 2,
         fallback = Vector( 10500, -9400, -16252 ),
         offsetX = 3500,
         offsetY = 3500
     }, 
     ["gm_genesis"] = {
+        centerZ = 27600 / 2 - 15300,
+        height = 27600 / 2,
         fallback = Vector( 6800, 6500, -14974 ),
         offsetX = 600,
         offsetY = 600
@@ -57,7 +70,14 @@ local MAP_POSITION_DATA = {
 
 local function planTrip( ply )
     for _ = 1, 20 do
-        local pos = Vector( math.random( MIN_X, MAX_X ), math.random( MIN_Y, MAX_Y ), math.random( MIN_Z, MAX_Z ) )
+        local mapData = MAP_POSITION_DATA[game.GetMap()]
+        local pos
+        
+        if mapData then
+            pos = Vector( math.random( MIN_X, MAX_X ), math.random( MIN_Y, MAX_Y ), mapData.centerZ + math.random( -mapData.height, mapData.height ) )
+        else
+            pos = Vector( math.random( MIN_X, MAX_X ), math.random( MIN_Y, MAX_Y ), math.random( MIN_Z, MAX_Z ) )
+        end
         
         if util.IsInWorld( pos ) then
             local minHullSize, maxHullSize = ply:GetCollisionBounds()
@@ -83,8 +103,6 @@ local function planTrip( ply )
             end
         end
     end
-    
-    local mapData = MAP_POSITION_DATA[game.GetMap()]
     
     if not mapData then return Vector( 0, 0, 0 ) end
     
