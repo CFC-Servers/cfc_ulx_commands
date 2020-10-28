47bfd9af35b95822d23a85d1fbe22caa88db4101
Banoargeek <thatguymajor@protonmail.com>
2020-10-28 13:20:54 -0600

    Change category type to Chat

--- lua/ulx/modules/sh/cfc_owoify.lua
+++ lua/ulx/modules/sh/cfc_owoify.lua
@@ -1,3 +1,4 @@
+local CATEGORY_TYPE = "Chat"
 local OWOIFYDICT = { na = "nya", ne = "nye", ni = "nyi", no = "nyo", nu = "nyu", ove = "uv", r = "w", l = "w", U = "uwu" }
 local owoifyTargets = {}
 
@@ -11,7 +12,7 @@ end
 
 local function onPlayerSay( ply, inputStr )
     if not table.HasValue( owoifyTargets, ply ) then return end
-    
+
     return owoifyString( inputStr )
 end
 
@@ -29,16 +30,16 @@ local function owoifyOff( caller, targets )
     if table.Count( owoifyTargets ) == 0 then
         hook.Remove( "PlayerSay", "CFC_ULX_OwoifyString" )
     end
-    
+
     ulx.fancyLogAdmin( caller, "#A unowoified #T", targets )
 end
 
-local owoifyCommand = ulx.command( "Fun", "ulx owoify", owoifyOn, "!owoify" )
+local owoifyCommand = ulx.command( CATEGORY_TYPE, "ulx owoify", owoifyOn, "!owoify" )
 owoifyCommand:defaultAccess( ULib.ACCESS_ADMIN )
 owoifyCommand:help( "Owoifies target(s) so they are unable to chat normally." )
 owoifyCommand:addParam{ type = ULib.cmds.PlayersArg }
 
-local unOwoifyCommand = ulx.command( "Fun", "ulx unowoify", owoifyOff, "!unowoify" )
+local unOwoifyCommand = ulx.command( CATEGORY_TYPE, "ulx unowoify", owoifyOff, "!unowoify" )
 unOwoifyCommand:defaultAccess( ULib.ACCESS_ADMIN )
 unOwoifyCommand:help( "Unowoifies target(s) so they are able to chat normally." )
 unOwoifyCommand:addParam{ type = ULib.cmds.PlayersArg }
