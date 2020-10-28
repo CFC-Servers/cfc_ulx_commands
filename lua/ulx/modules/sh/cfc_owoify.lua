85c6b61de6dcec5a87d4669a541165f17e0aa1a8
Banoargeek <thatguymajor@protonmail.com>
2020-10-28 02:10:24 -0600

    fix owoify functions, add ULX framework

--- lua/ulx/modules/sh/cfc_owoify.lua
+++ lua/ulx/modules/sh/cfc_owoify.lua
@@ -1,31 +1,42 @@
 local OWOIFYDICT = { na = "nya", ne = "nye", ni = "nyi", no = "nyo", nu = "nyu", ove = "uv", r = "w", l = "w", U = "uwu" }
+local owoifyTargets = {}
 
 local function owoifyString( inputStr )
-	for k, v in pairs( OWOIFYDICT ) do
-		inputStr = string.Replace( inputStr, k, v )
+    for k, v in pairs( OWOIFYDICT ) do
+        inputStr = string.Replace( inputStr, k, v )
+    end
+
+    return inputStr
+end
+
+local function onPlayerSay( ply, inputStr )
+    return owoifyString( inputStr )
+end
+
+local function owoifyOn( caller, targets )
+    table.Inherit( owoifyTargets, targets )
+    ulx.fancyLogAdmin( caller, "#A owoified #T", targets )
+    hook.Add( "PlayerSay", "CFC_ULX_OwoifyString", onPlayerSay )
+end
+
+local function owoifyOff( caller, targets )
+	for k, v in pairs( owoifyDict ) do
+		table.remove( k )
 	end
-	
-	return inputStr
+
+	if table.Count( owoifyTargets ) == 0 then
+		hook.Remove( "PlayerSay", "CFC_ULX_OwoifyString" )
+	end
+
+    ulx.fancyLogAdmin( caller, "#A unowoified #T", targets )
 end
 
-local function onPlayerSay( ply, inputStr )	
-	return owoifyString( inputStr )
-end
-
-local function stopULX_Owoify()
-	print( "~ [ULX_Owoify]: Removing hook from server!`" )
-	hook.Remove( "PlayerSay", "CFC_ULX_OwoifyString" )
-end
-	
-hook.Add( "PlayerSay", "CFC_ULX_OwoifyString", onPlayerSay )
-concommand.Add( "owoify_stop", stopULX_Owoify )
-
-local owoifyCommand = ulx.command( "Fun", "ulx owoify", onPlayerSay, "!owoify" )
+local owoifyCommand = ulx.command( "Fun", "ulx owoify", owoifyOn, "!owoify" )
 owoifyCommand:defaultAccess( ULib.ACCESS_ADMIN )
-owoifyCommand:addParam{ type = ULib.cmds.PlayersArg }
 owoifyCommand:help( "Owoifies target(s) so they are unable to chat normally." )
-
-local owoifyCommand = ulx.command( "Fun", "ulx unowoify", onPlayerSay, "!unowoify" )
-owoifyCommand:defaultAccess( ULib.ACCESS_ADMIN )
 owoifyCommand:addParam{ type = ULib.cmds.PlayersArg }
-owoifyCommand:help( "Unowoifies target(s) so they are able to chat normally." )
+
+local unOwoifyCommand = ulx.command( "Fun", "ulx unowoify", owoifyOff, "!unowoify" )
+unOwoifyCommand:defaultAccess( ULib.ACCESS_ADMIN )
+unOwoifyCommand:help( "Unowoifies target(s) so they are able to chat normally." )
+unOwoifyCommand:addParam{ type = ULib.cmds.PlayersArg }
