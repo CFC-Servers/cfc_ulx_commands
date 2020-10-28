15eaa8c137c053bb3ec57b4177591e25b439cd90
legokidlogan <legokidlogan@gmail.com>
2020-10-27 21:41:28 -0600

    Fix syntax errors

--- lua/ulx/modules/sh/cfc_brazil.lua
+++ lua/ulx/modules/sh/cfc_brazil.lua
@@ -11,7 +11,8 @@ local HULL_SIZE = Vector( 32, 32, 72 )
 local function sendToBrazil( players )
 	for k, v in pairs( players ) do
 		local isValid, pos = tripAdvise()
-		if isValid do
+		
+		if isValid then
 			
 		else
 			
@@ -23,10 +24,10 @@ local function tripAdvise()
 	for i = 1, 50 do
 		local pos = Vector( math.random( MIN_X, MAX_X ), math.random( MIN_Y, MAX_Y ), math.random( MIN_Z, MAX_Z )
 		
-		if not util.IsInWorld( pos ) do
+		if not util.IsInWorld( pos ) then
 			local traceBox = util.HullTrace( pos, pos + Vector( 0, 0, -HULL_SIZE.z ), Vector( HULL_SIZE.x, HULL_SIZE.y, 0.1 ), Vector( -HULL_SIZE.x, -HULL_SIZE.y, -0.1 ) )
 			
-			if not traceBox["Hit"] do
+			if not traceBox["Hit"] then
 				local trace = util.TraceLine( pos, pos + vec( 0, 0, MIN_Z )
 				
 				return true, trace["HitPos"] + Vector( 0, 0, 1 )
