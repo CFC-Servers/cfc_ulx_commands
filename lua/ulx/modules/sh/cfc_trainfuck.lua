214da95f79b33d8b79c9abac8d028c348a794a6d
wrefgtzweve <jasperdk@planet.nl>
2020-10-29 06:07:00 +0100

    Fixed style and timer like suggested.

--- lua/ulx/modules/sh/cfc_trainfuck.lua
+++ lua/ulx/modules/sh/cfc_trainfuck.lua
@@ -1,26 +1,24 @@
 CATEGORY_NAME = "Fun"
 
 local function trainFuck( callingPlayer, targetPlayers )
-	local Y = 1
-	for _, ply in pairs( targetPlayers ) do
-		local train = ents.Create("prop_physics")
-		train:SetModel("models/props_trainstation/train001.mdl")
-		train:SetPos(ply:GetPos()+Vector(0,-1000,300))
+	for _,ply in pairs(targetPlayers) do
+		local train = ents.Create( "prop_physics" )
+		train:SetModel( "models/props_trainstation/train001.mdl" )
+		train:SetPos( ply:GetPos() + Vector( 0, -1000, 300 ) )
 		train:Spawn()
 		
 		local phys = train:GetPhysicsObject()
 		
-		if (IsValid(phys)) then
-			if ply:GetMoveType(MOVETYPE_NOCLIP) then
-				ply:SetMoveType(MOVETYPE_WALK)
+		if IsValid( phys ) then
+			if ply:GetMoveType( MOVETYPE_NOCLIP ) then
+				ply:SetMoveType( MOVETYPE_WALK )
 			end
 			ply:ExitVehicle()
-			phys:ApplyForceCenter((ply:GetPos()-train:GetPos())*1000000000)
-			timer.Create("trainFuckTimer"..Y,1.5,1,function()
+			phys:ApplyForceCenter( ( ply:GetPos() - train:GetPos() ) * 1000000000 )
+			timer.Simple( 1.5, function()
 				train:Remove()
 			end)
 		end
-		Y = Y + 1
 	end
 	ulx.fancyLogAdmin( callingPlayer, "#A trainfucked #T", targetPlayers )
 end
