3adf66dfafa54ba8ecc7c3c6bfbfde793e6fd8d6
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-10-29 22:29:43 +0100

    Fix style and tab to space

--- lua/ulx/modules/sh/cfc_trainfuck.lua
+++ lua/ulx/modules/sh/cfc_trainfuck.lua
@@ -1,26 +1,26 @@
 CATEGORY_NAME = "Fun"
 
 local function trainFuck( callingPlayer, targetPlayers )
-	for _, ply in pairs(targetPlayers) do
-		local train = ents.Create( "prop_physics" )
-		train:SetModel( "models/props_trainstation/train001.mdl" )
-		train:SetPos( ply:GetPos() + Vector( 0, -1000, 300 ) )
-		train:Spawn()
-		
-		local phys = train:GetPhysicsObject()
-		
-		if IsValid( phys ) then
-			if ply:GetMoveType( MOVETYPE_NOCLIP ) then
-				ply:SetMoveType( MOVETYPE_WALK )
-			end
-			ply:ExitVehicle()
-			phys:ApplyForceCenter( ( ply:GetPos() - train:GetPos() ) * 1000000000 )
-			timer.Simple( 1.5, function()
-				train:Remove()
-			end)
-		end
-	end
-	ulx.fancyLogAdmin( callingPlayer, "#A trainfucked #T", targetPlayers )
+    for _, ply in pairs(	targetPlayers   ) do
+        local train = ents.Create( "prop_physics" )
+        train:SetModel( "models/props_trainstation/train001.mdl" )
+        train:SetPos( ply:GetPos() + Vector( 0, -1000, 300 ) )
+        train:Spawn()
+
+        local phys = train:GetPhysicsObject()
+
+        if IsValid( phys ) then
+            if ply:GetMoveType( MOVETYPE_NOCLIP ) then
+                ply:SetMoveType( MOVETYPE_WALK )
+            end
+            ply:ExitVehicle()
+            phys:ApplyForceCenter( ( ply:GetPos() - train:GetPos() ) * 1000000000 )
+            timer.Simple( 1.5, function()
+                train:Remove()
+            end)
+        end
+    end
+    ulx.fancyLogAdmin( callingPlayer, "#A trainfucked #T", targetPlayers )
 end
 
 local trainfuckCMD = ulx.command( CATEGORY_NAME, "ulx trainfuck", trainFuck, "!trainfuck" )
