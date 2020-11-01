3af84d6e4c88e4fc0c7c1e93bae68076aad57d7c
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-11-01 23:17:27 +0100

    Apply suggestions from code review
    Co-authored-by: Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>

--- lua/entities/train_fucked/init.lua
+++ lua/entities/train_fucked/init.lua
@@ -3,10 +3,9 @@ AddCSLuaFile( "shared.lua" )  -- and shared scripts are sent.
 include('shared.lua')
  
 function ENT:Initialize()
-
-	self:SetModel( "models/props_trainstation/train001.mdl" )
-	self:PhysicsInit( SOLID_VPHYSICS )
-	self:SetSolid( SOLID_VPHYSICS )
+    self:SetModel( "models/props_trainstation/train001.mdl" )
+    self:PhysicsInit( SOLID_VPHYSICS )
+    self:SetSolid( SOLID_VPHYSICS )
     self:SetCollisionGroup( COLLISION_GROUP_INTERACTIVE_DEBRIS )
     self:PhysWake()
     
