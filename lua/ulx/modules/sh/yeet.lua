6cd232425fc9f2d6acb6070ae039c5b0c368f75b
SimplyMidnight <thelordmidnight0202@gmail.com>
2020-11-02 16:51:27 -0700

    Fix format

--- lua/ulx/modules/sh/yeet.lua
+++ lua/ulx/modules/sh/yeet.lua
@@ -6,33 +6,33 @@ local DEFAULT_STRENGTH = 6000
 
 function yeetPlayer( caller, targets, strength )
 	
-	for _, v in pairs( targets ) do 
+    for _, v in pairs( targets ) do
 		
-	if not v:IsPlayer() then return end
-	if not v:Alive() then return end
+    if not v:IsPlayer() then return end
+    if not v:Alive() then return end
 
-	if v:IsFrozen() then
+    if v:IsFrozen() then
 		
-	v:Freeze( false )
+   	    v:Freeze( false )
 					
-	end
+    end
 	
-	if v:GetMoveType() == MOVETYPE_NOCLIP then
+    if v:GetMoveType() == MOVETYPE_NOCLIP then
 			
-	v:SetMoveType( MOVETYPE_WALK )
+        v:SetMoveType( MOVETYPE_WALK )
 			
-	end
+    end
 
-	v:SetVelocity( Vector( math.random( 50000 ) -20000, math.random( 50000 ) -20000, math.Clamp( strength, MIN_STRENGTH, MAX_STRENGTH ) or DEFAULT_STRENGTH ) )  
+    v:SetVelocity( Vector( math.random( 50000 ) -20000, math.random( 50000 ) -20000, math.Clamp( strength, MIN_STRENGTH, MAX_STRENGTH ) or DEFAULT_STRENGTH ) )  
 		
-	end
-	
-	ulx.fancyLogAdmin( caller, "#A yeeted #T with #i strength", targets, strength )
+    end
+
+    ulx.fancyLogAdmin( caller, "#A yeeted #T with #i strength", targets, strength )
 		
 end
 
 local yeet = ulx.command( CATEGORY_NAME, "ulx yeet", yeetPlayer, "!yeet" )
-yeet:addParam{ type=ULib.cmds.PlayersArg }
-yeet:addParam{ type=ULib.cmds.NumArg, min = MIN_STRENGTH, default = DEFAULT_STRENGTH, max = MAX_STRENGTH, ULib.cmds.optional }
+yeet:addParam{ type = ULib.cmds.PlayersArg }
+yeet:addParam{ type = ULib.cmds.NumArg, min = DEFAULT_STRENGTH, default = DEFAULT_STRENGTH, max = MAX_STRENGTH, ULib.cmds.optional }
 yeet:defaultAccess( ULib.ACCESS_ADMIN )
 yeet:help( "Yeets the player(s) with the given strength." )
