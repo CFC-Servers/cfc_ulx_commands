218334e252d6c3018a8535d0533eb1533ca58e9d
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-11-02 01:21:34 +0100

    Apply suggestions from code review
    Co-authored-by: Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>

--- lua/entities/train_fucked/init.lua
+++ lua/entities/train_fucked/init.lua
@@ -1,6 +1,6 @@
 AddCSLuaFile( "cl_init.lua" ) -- Make sure clientside
 AddCSLuaFile( "shared.lua" )  -- and shared scripts are sent.
-include('shared.lua')
+include( "shared.lua" )
  
 function ENT:Initialize()
     self:SetModel( "models/props_trainstation/train001.mdl" )
