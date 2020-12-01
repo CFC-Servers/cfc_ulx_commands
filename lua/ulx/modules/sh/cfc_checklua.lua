eb05421f04acf0890c0b4f6fc376449aa605df9c
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-12-01 20:39:04 +0100

    Cleanup and style

--- lua/ulx/modules/sh/cfc_checklua.lua
+++ lua/ulx/modules/sh/cfc_checklua.lua
@@ -3,28 +3,23 @@ local cmd = CFCUlxCommands.checklua
 
 CATEGORY_NAME = "Utility"
 
-local callingPlayer
-local targetPlayerss
-
 local function askForConvar( ply )
     ply.waitingOnVar = true
     net.Start( "CFC_ULX_StatCheckCL" )
     net.Send( ply )
 end
-
-net.Receive( "CFC_ULX_StatCheckSV", function( _, ply )
-    if not ply.waitingOnVar then return end
-    local var = net.ReadBool()
-    ply.waitingOnVar = false
-    ulx.fancyLogAdmin( callingPlayer, true, "#T's sv_allowcslua value is " ..  tostring( var ), targetPlayerss )
-end )
                     
-function cmd.checkluaPlayers( calPlayer, targetPlayers )
+function cmd.checkluaPlayers( callingPlayer, targetPlayers )
     for _, ply in pairs( targetPlayers ) do
-        callingPlayer = calPlayer
-        targetPlayerss = targetPlayers
         askForConvar( ply )
     end
+
+    net.Receive( "CFC_ULX_StatCheckSV", function( _, ply )
+        if not ply.waitingOnVar then return end
+        local var = net.ReadBool()
+        ply.waitingOnVar = false
+        ulx.fancyLogAdmin( callingPlayer, true, "#T's sv_allowcslua value is " .. tostring( var ), targetPlayers )
+    end )
 end
 
 local checkluaCommand = ulx.command( CATEGORY_NAME, "ulx checklua", cmd.checkluaPlayers, "!checklua" )
@@ -34,7 +29,7 @@ checkluaCommand:help( "Checks target(s) sv_allowcslua" )
 
 if CLIENT then
     net.Receive( "CFC_ULX_StatCheckCL", function()
-        local convarBool = GetConVar("sv_allowcslua"):GetBool()
+        local convarBool = GetConVar( "sv_allowcslua" ):GetBool()
         net.Start( "CFC_ULX_StatCheckSV" )
         net.WriteBool( convarBool )
         net.SendToServer()
