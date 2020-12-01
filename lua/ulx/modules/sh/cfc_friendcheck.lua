2351deb5b1ead7eab43906198ab774555321bd44
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-12-01 23:14:03 +0100

    Initial friendcheck file

--- lua/ulx/modules/sh/cfc_friendcheck.lua
+++ lua/ulx/modules/sh/cfc_friendcheck.lua
@@ -1,16 +1,52 @@
 CFCUlxCommands.friendcheck = CFCUlxCommands.friendcheck or {}
 local cmd = CFCUlxCommands.friendcheck
 
-CATEGORY_NAME = "Utilities"
-                    
+CATEGORY_NAME = "Utility"
+
+if SERVER then
+    util.AddNetworkString( "CFC_ULX_FriendCheckSend" )
+    util.AddNetworkString( "CFC_ULX_FriendCheckRecieve" )
+end
+
+local awaitingResponse = {}
+
+net.Receive( "CFC_ULX_FriendCheckRecieve", function( _, ply )
+    if not ply.waitingOnFriendCheck then return end
+    local friendTable = net.ReadTable()
+    ply.waitingOnFriendCheck = false
+
+    PrintTable( friendTable )
+    ulx.fancyLogAdmin( awaitingResponse[ply], true, "#T's sv_allowcslua value is " .. tostring( convar ), ply )
+
+    awaitingResponse[ply] = nil
+end )
+
 function cmd.friendcheckPlayers( callingPlayer, targetPlayers )
     for _, ply in pairs( targetPlayers ) do
+        ply.waitingOnFriendCheck = true
+        awaitingResponse[ply] = callingPlayer
+        net.Start( "CFC_ULX_FriendCheckSend" )
+        net.Send( ply )
     end
+end
 
-    ulx.fancyLogAdmin( callingPlayer, "#A friendcheck #T", targetPlayers )
+if CLIENT then
+    net.Receive( "CFC_ULX_FriendCheckSend", function()
+        local friendTable
+
+        for _, ply in pairs( player.GetHumans() ) do
+            local friendStatus = ply:GetFriendStatus()
+            if friendStatus == "none" then return end
+            table.insert( friendTable, ply, friendStatus )
+        end
+
+        net.Start( "CFC_ULX_FriendCheckRecieve" )
+        net.WriteTable( friendTable )
+        net.SendToServer()
+    end )
 end
 
 local friendcheckCommand = ulx.command( CATEGORY_NAME, "ulx friendcheck", cmd.friendcheckPlayers, "!friendcheck" )
 friendcheckCommand:addParam{ type = ULib.cmds.PlayersArg }
 friendcheckCommand:defaultAccess( ULib.ACCESS_ADMIN )
-friendcheckCommand:help( "friendchecks target(s)" )
+friendcheckCommand:help( "Checks the targetted player(s) friends." )
