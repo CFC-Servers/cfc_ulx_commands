b3d7d5657b2e3cea8973522ea1d8a4bfe2273c5b
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-12-02 18:13:39 +0100

    Change % instead of multiplier

--- lua/ulx/modules/sh/cfc_speed.lua
+++ lua/ulx/modules/sh/cfc_speed.lua
@@ -5,15 +5,15 @@ CATEGORY_NAME = "Fun"
                     
 function cmd.speedPlayers( callingPlayer, targetPlayers, amount )
     for _, ply in pairs( targetPlayers ) do
-        ply:SetWalkSpeed( 200 * amount )
-        ply:SetRunSpeed( 400 * amount )
+        ply:SetWalkSpeed( 2 * amount )
+        ply:SetRunSpeed( 4 * amount )
     end
 
-    ulx.fancyLogAdmin( callingPlayer, "#A set the speed for #T to a " .. amount .. " speed multiplier", targetPlayers )
+    ulx.fancyLogAdmin( callingPlayer, "#A set the speed for #T to " .. amount .. "% speed", targetPlayers )
 end
 
 local speedCommand = ulx.command( CATEGORY_NAME, "ulx speed", cmd.speedPlayers, "!speed" )
 speedCommand:addParam{ type = ULib.cmds.PlayersArg }
-speedCommand:addParam{ type=ULib.cmds.NumArg, min=1, max=10, hint="speed", ULib.cmds.round }
+speedCommand:addParam{ type=ULib.cmds.NumArg, min=1, max=1000, default=100, hint="speed %", ULib.cmds.round, ULib.cmds.optional }
 speedCommand:defaultAccess( ULib.ACCESS_ADMIN )
-speedCommand:help( "Sets the speed for target(s)." )
+speedCommand:help( "Sets the speed percentage for target(s) default = 100." )
