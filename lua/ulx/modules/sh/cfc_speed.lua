80bdf2d444f2c18ee46f7b1225053a9d9d748c79
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-12-02 17:18:30 +0100

    Style

--- lua/ulx/modules/sh/cfc_speed.lua
+++ lua/ulx/modules/sh/cfc_speed.lua
@@ -5,15 +5,15 @@ CATEGORY_NAME = "Fun"
                     
 function cmd.speedPlayers( callingPlayer, targetPlayers, amount )
     for _, ply in pairs( targetPlayers ) do
-        ply:SetWalkSpeed( 400 )
-        ply:SetRunSpeed( 600 )
+        ply:SetWalkSpeed( 200 * amount )
+        ply:SetRunSpeed( 400 * amount )
     end
 
-    ulx.fancyLogAdmin( callingPlayer, "#A set the speed for #T to #i", targetPlayers )
+    ulx.fancyLogAdmin( callingPlayer, "#A set the speed for #T to a " .. amount .. " speed multiplier", targetPlayers )
 end
 
 local speedCommand = ulx.command( CATEGORY_NAME, "ulx speed", cmd.speedPlayers, "!speed" )
 speedCommand:addParam{ type = ULib.cmds.PlayersArg }
-speedCommand:addParam{ type=ULib.cmds.NumArg, min=1, max=2^32/2-1, hint="speed", ULib.cmds.round }
+speedCommand:addParam{ type=ULib.cmds.NumArg, min=1, max=10, hint="speed", ULib.cmds.round }
 speedCommand:defaultAccess( ULib.ACCESS_ADMIN )
 speedCommand:help( "Sets the speed for target(s)." )
