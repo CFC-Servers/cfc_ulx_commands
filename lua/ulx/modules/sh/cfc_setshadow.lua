f6bf3abe8cea0fa786a8f8a58635463d99f26927
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-12-31 14:45:58 +0100

    Initial

---
+++ lua/ulx/modules/sh/cfc_setshadow.lua
@@ -0,0 +1,39 @@
+CFCUlxCommands.freezeProps = CFCUlxCommands.freezeProps or {}
+local cmd = CFCUlxCommands.freezeProps
+
+CATEGORY_NAME = "Cleanup"
+
+function cmd.freezeProps( callingPlayer, targetPlayers )
+    local entities = ents.GetAll()
+    local entCount = 0
+    local entCounts = {}
+    for _, ply in pairs( targetPlayers ) do
+        entCounts[ply] = 0
+    end
+
+    for _, ent in pairs( entities ) do
+        local owner = ent.CPPIGetOwner and ent:CPPIGetOwner()
+        if owner and entCounts[owner] then
+            local canFreeze = not ( ent:IsWeapon() or ent:GetUnFreezable() or ent:IsPlayer() )
+            local physicsObj = ent:GetPhysicsObject()
+            if IsValid( physicsObj ) and canFreeze then
+                physicsObj:EnableMotion( false )
+                physicsObj:Sleep()
+                entCount = entCount + 1
+                entCounts[owner] = entCounts[owner] + 1
+            end
+        end
+    end
+    ulx.fancyLogAdmin( callingPlayer, "#A froze " .. entCount .. " props owned by #T", targetPlayers )
+
+    if #targetPlayers <= 1 then return end
+
+    for ply, num in pairs( entCounts ) do
+        ULib.tsay( ply, ply:Nick() .. " owned " .. num .. " props.", true )
+    end
+end
+
+local freezeCMD = ulx.command( CATEGORY_NAME, "ulx freezeprops", cmd.freezeProps, "!freezeprops" )
+freezeCMD:addParam{ type = ULib.cmds.PlayersArg }
+freezeCMD:defaultAccess( ULib.ACCESS_ADMIN )
+freezeCMD:help( "Freezes target( s ) props" )
