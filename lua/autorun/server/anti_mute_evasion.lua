364939e8d9126fdd6c436f0c583821a466f46986
plally <11771225+plally@users.noreply.github.com>
2021-01-21 01:16:41 -0500

    prevent users from  using gimp and owoify commands while muted

---
+++ lua/autorun/server/anti_mute_evasion.lua
@@ -0,0 +1,16 @@
+local ID_MUTE = 2
+
+local isBannedWhileMuted = {
+	["ulx ungimp"] = true,
+	["ulx gimp"] = true,
+	["ulx unowoify"] = true,
+	["ulx owoify"] = true,
+}
+
+hook.Add( "ULibCommandCalled", "CFC_AntiUnmute_PreventUnmute", function( ply, commandName, args )
+	if not isBannedWhileMuted[commandName] then return end
+	if ply.gimp == ID_MUTE then
+		ULib.tsayError( ply, "you can't do that while muted" )
+		return false
+	end
+end )
