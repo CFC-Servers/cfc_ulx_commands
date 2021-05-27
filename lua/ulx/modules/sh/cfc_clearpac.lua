8bdba7d1948c045ff903660878a1af5eafee930a
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-05-27 08:20:29 +0200

    Add ulx clearpac command
    Used for when a player is abusing pac so staff can clear their pac. This is written in the browser didn't test it yet

---
+++ lua/ulx/modules/sh/cfc_clearpac.lua
@@ -0,0 +1,17 @@
+CFCUlxCommands.clearpac = CFCUlxCommands.clearpac or {}
+local cmd = CFCUlxCommands.clearpac
+
+local CATEGORY_NAME = "Utility"
+                    
+function cmd.clearPac( callingPlayer, targetPlayers, amount )
+    for _, ply in pairs( targetPlayers ) do
+        ply:ConCommand( "pac_clear_parts" )
+    end
+
+    ulx.fancyLogAdmin( callingPlayer, "#A cleared #T's pac", targetPlayers )
+end
+
+local clearpacCommand = ulx.command( CATEGORY_NAME, "ulx clearpac", cmd.clearPac, "!clearpac" )
+clearpacCommand:addParam{ type = ULib.cmds.PlayersArg }
+clearpacCommand:defaultAccess( ULib.ACCESS_ADMIN )
+clearpacCommand:help( "Clear the pac for the target(s)." )
