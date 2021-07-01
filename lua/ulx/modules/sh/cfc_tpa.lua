0591cbe99ae71e9e9f4afba7a3c00f1236646d81
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-07-02 01:38:31 +0200

    Implemented cooldown system

--- lua/ulx/modules/sh/cfc_tpa.lua
+++ lua/ulx/modules/sh/cfc_tpa.lua
@@ -2,8 +2,28 @@ CFCUlxCommands.tpa = CFCUlxCommands.tpa or {}
 local cmd = CFCUlxCommands.tpa
 local CATEGORY_NAME = "Teleport"
 
+CreateConVar( "cfc_tpa_decline_cooldown", 10, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "The time a person can't receive teleports from a player after declining.", 0 )
+CreateConVar( "cfc_tpa_cooldown", 5, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "The cooldown between tpa request for any player.", 0 )
+
+local function setDeclineCooldown( caller, target )
+    target.cfcTpaCooldownDecline = target.cfcTpaCooldownDecline or {}
+    target.cfcTpaCooldownDecline[ caller ] = CurTime() + GetConVar( "cfc_tpa_decline_cooldown" ):GetInt()
+end
+
 function cmd.tpa( callingPlayer, targetPlayers )
     local target = targetPlayers[1]
+
+    if target.cfcTpaCooldownDecline and target.cfcTpaCooldownDecline[ callingPlayer ] and target.cfcTpaCooldownDecline[ callingPlayer ] > CurTime() then
+        CFCNotifications.sendSimple( "tpaDeclineCooldown", "TPA", "You cannot request a TPA to that player yet.", callingPlayer )
+        return
+    end
+
+    if callingPlayer.cfcTpaCooldown and callingPlayer.cfcTpaCooldown > CurTime() then
+        CFCNotifications.sendSimple( "tpaCooldown", "TPA", "You cannot send another teleport request yet.", callingPlayer )
+        return
+    end
+
+    callingPlayer.cfcTpaCooldown = CurTime() + GetConVar( "cfc_tpa_cooldown" ):GetInt()
 
     CFCNotifications.sendSimple( "tpaNotifCaller", "TPA", "Send teleport request.", callingPlayer )
 
@@ -24,8 +44,8 @@ function cmd.tpa( callingPlayer, targetPlayers )
             CFCNotifications.sendSimple( "tpaClose", "TPA", "Teleport request was accepted.", callingPlayer )
         else
             CFCNotifications.sendSimple( "tpaClose", "TPA", "The recipient has denied your teleport request.", callingPlayer )
+            setDeclineCooldown( callingPlayer, target )
         end
-        -- Do stuff based on ind being 1 or 2
     end
 
     function notif:OnClose( wasTimeout )
@@ -33,17 +53,11 @@ function cmd.tpa( callingPlayer, targetPlayers )
             CFCNotifications.sendSimple( "tpaTimeout", "TPA", "The recipient didn't accept within the given time.", callingPlayer )
         elseif not wasTimeout then
             CFCNotifications.sendSimple( "tpaClose", "TPA", "The recipient has denied your teleport request.", callingPlayer )
+            setDeclineCooldown( callingPlayer, target )
         end
-        -- Do stuff
     end
 
     notif:Send( target )
-
-    -- for _, ply in ipairs( targetPlayers ) do
-    --     -- do code
-    -- end
-
-    --ulx.fancyLogAdmin( callingPlayer, "#A requested teleportation to #T", targetPlayers )
 end
 
 local tpaCommand = ulx.command( CATEGORY_NAME, "ulx tpa", cmd.tpa, "!tpa" )
