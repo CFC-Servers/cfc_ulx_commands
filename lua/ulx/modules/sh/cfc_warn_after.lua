0e7c8f55591133de9c6bb251f9a23af0e58205de
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-07-02 02:55:29 +0200

    warn_after remove whitespaces

--- lua/ulx/modules/sh/cfc_warn_after.lua
+++ lua/ulx/modules/sh/cfc_warn_after.lua
@@ -5,8 +5,8 @@ local CATEGORY_NAME = "Utility"
 
 function cmd.warnBan( calling_ply, target_ply, minutes, reason )
     ulx.ban( calling_ply, target_ply, minutes, reason )
-    local command = string.format( 'awarn_warn \"%s" "%s (%s ban)" ', target_ply:SteamID(), reason, ULib.secondsToStringTime( minutes*60 ) )
-    calling_ply:ConCommand( command ) 
+    local command = string.format( 'awarn_warn \"%s" "%s (%s ban)" ', target_ply:SteamID(), reason, ULib.secondsToStringTime( minutes * 60 ) )
+    calling_ply:ConCommand( command )
 end
 local ban = ulx.command( CATEGORY_NAME, "ulx warnban", cmd.warnBan, "!warnban", false, false, true )
 ban:addParam{ type = ULib.cmds.PlayerArg }
@@ -18,7 +18,7 @@ ban:help( "Bans target and warns them." )
 function cmd.warnKick( calling_ply, target_ply, reason )
     ulx.kick( calling_ply, target_ply, reason )
     local command = string.format( 'awarn_warn \"%s" "%s (kick)" ', target_ply:SteamID(), reason )
-    calling_ply:ConCommand( command ) 
+    calling_ply:ConCommand( command )
 end
 local kick = ulx.command( CATEGORY_NAME, "ulx warnkick", cmd.warnKick, "!warnkick" )
 kick:addParam{ type = ULib.cmds.PlayerArg }
