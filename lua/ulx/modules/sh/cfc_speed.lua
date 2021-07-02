80ddbf17aaddbfbbab4df209220aa25b032c2c31
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-07-02 02:54:11 +0200

    speed fix style

--- lua/ulx/modules/sh/cfc_speed.lua
+++ lua/ulx/modules/sh/cfc_speed.lua
@@ -2,7 +2,7 @@ CFCUlxCommands.speed = CFCUlxCommands.speed or {}
 local cmd = CFCUlxCommands.speed
 
 local CATEGORY_NAME = "Fun"
-                    
+
 function cmd.speedPlayers( callingPlayer, targetPlayers, amount )
     for _, ply in ipairs( targetPlayers ) do
         local inPvp = hook.Run( "CFC_ULXCommands_CanApplySpeedModifier", ply )
@@ -18,6 +18,6 @@ end
 
 local speedCommand = ulx.command( CATEGORY_NAME, "ulx speed", cmd.speedPlayers, "!speed" )
 speedCommand:addParam{ type = ULib.cmds.PlayersArg }
-speedCommand:addParam{ type=ULib.cmds.NumArg, min=1, max=1000, default=100, hint="speed %", ULib.cmds.round, ULib.cmds.optional }
+speedCommand:addParam{ type = ULib.cmds.NumArg, min = 1, max = 1000, default = 100, hint = "speed %", ULib.cmds.round, ULib.cmds.optional }
 speedCommand:defaultAccess( ULib.ACCESS_ADMIN )
 speedCommand:help( "Sets the speed percentage for target(s) default = 100." )
