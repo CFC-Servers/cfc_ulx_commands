889664d5eb552762d29c1111d59f0e6858aabbe0
plally <pierce@vulpes.dev>
2021-07-17 17:31:36 -0400

    Apply suggestions from code review
    Co-authored-by: Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>

--- lua/autorun/server/cfc_auto_warn_on_commands.lua
+++ lua/autorun/server/cfc_auto_warn_on_commands.lua
@@ -6,22 +6,22 @@ local autoWarnCommands = {
     }	
 }
 
-hook.Add("ULibPostTranslatedCommand", "CFC_AutoWarn_WarnOnCommands", function( caller, commandName, args )
+hook.Add( "ULibPostTranslatedCommand", "CFC_AutoWarn_WarnOnCommands", function( caller, commandName, args )
     local indexes = autoWarnCommands[commandName]
     if not indexes then return end
     
     local targets
     if indexes.targets then
         targets = args[indexes.targets]
-    elseif indexes.target
-        targets  = {args[indexes.target]}
+    elseif indexes.target then
+        targets = { args[indexes.target] }
     end
     
-    local duration = args[3]
-    local reason = args[4]
+    local duration = args[indexes.duration]
+    local reason = args[indexes.reason]
 
-    for _, target in pairs(targets) do
-        local command = string.format( 'awarn_warn \"%s" "%s (%s %s)" ', target:SteamID(), reason, commandName, ULib.secondsToStringTime( duration * 60 ) )
+    for _, target in pairs( targets ) do
+        local command = string.format( 'awarn_warn \"%s" "%s (%s, %s)"', target:SteamID(), reason, commandName, ULib.secondsToStringTime( duration * 60 ) )
         caller:ConCommand( command )
     end
-end)
+end )
