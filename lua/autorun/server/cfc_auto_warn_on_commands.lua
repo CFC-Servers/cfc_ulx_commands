92c3c2304b01fe57f905cb0b00a340b865deaf99
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-12-10 19:21:11 -0800

    wip

--- lua/autorun/server/cfc_auto_warn_on_commands.lua
+++ lua/autorun/server/cfc_auto_warn_on_commands.lua
@@ -1,75 +1,91 @@
-local commandArgumentIndexes = {
-    ["ulx timegag"] = {
-        targets = 2,
-        duration = 3,
-        reason = 4,
-        dontWarnOnEmptyReason = true
-    },
-    ["ulx timemute"] = {
-        targets = 2,
-        duration = 3,
-        reason = 4
-    },
-    ["ulx pvpban"] = {
-        targets = 2,
-        duration = 3,
-        reason = 4
-    },
-    ["ulx ban"] = {
-        target = 2,
-        duration = 3,
-        reason = 4
-    },
-    ["ulx banid"] = {
-        targetId = 2,
-        duration = 3,
-        reason = 4
-    }
+local IsValid = IsValid
+local istable = istable
+local isentity = isentity
+local concat = table.concat
+
+local defaultArgIndices = {
+    targets = 2,
+    duraiton = 3,
+    reason = 4
 }
 
+local enabledCommands = {
+    ["ulx timegag"] = {
+        dontWarnOnEmptyReason = true
+    },
+    ["ulx timemute"] = {},
+    ["ulx pvpban"] = {},
+    ["ulx ban"] = {},
+    ["ulx banid"] = {}
+}
+
+local function buildReason( reason, commandName, duration )
+    local info = commandName
+
+    if duration then
+        info = info .. " " .. ULib.secondsToStringTime( duration )
+    end
+
+    info = "(" .. info .. ")"
+    reason = reason .. " " .. info
+
+    return reason
+end
+
+local function quoteWrap( s )
+    return '"' .. s .. '"'
+end
+
+local function warn( caller, target, reason )
+    if isentity( target ) then
+        target = target:SteamID()
+    end
+
+    awarn_warnplayerid( caller, target, reason )
+end
+
+local function getTargets( indices, args )
+    local targets = args[indices.targets]
+
+    if not istable( targets ) then
+        targets = { targets }
+    end
+
+    return targets
+end
+
+local function shouldWarn( cmd, duration, reason )
+    if cmd.dontWarnOnEmptyReason then
+        if not reason or reason == "" then return false end
+    end
+
+    if cmd.minDuration then
+        if reason > cmd.minDuration then return false end
+    end
+
+    return true
+end
+
+local function parseCommand( cmd, args )
+    local indices = cmd.indices or defaultArgIndices
+
+    local duration = args[indices.duration]
+    local reason = args[indices.reason]
+    local targets = getTargets( indices, args )
+
+    return duration, reason, targets
+end
+
 hook.Add( "ULibPostTranslatedCommand", "CFC_AutoWarn_WarnOnCommands", function( caller, commandName, args )
-    local indexes = commandArgumentIndexes[commandName]
-    if not indexes then return end
+    local cmd = enabledCommands[commandName]
+    if not cmd then return end
 
-    local duration = args[indexes.duration]
-    local reason = args[indexes.reason]
-    local minDuration = indexes.minDuration
+    local duration, reason, targets = parseCommand( cmd, args )
 
-    if indexes.dontWarnOnEmptyReason then
-        if not reason or reason == "" then return end
-    end
+    if not shouldWarn( cmd, duration, reason ) then return end
 
-    local targets
-    if indexes.targets then
-        targets = args[indexes.targets]
-    elseif indexes.target then
-        targets = { args[indexes.target] }
-    elseif indexes.targetId then
-        local targetId = args[indexes.targetId]
-
-        local stringTime = ULib.secondsToStringTime( duration * 60 )
-        local reasonArg = string.format( '%s (%s %s)', reason, commandName, stringTime )
-
-        if IsValid( caller ) then
-            local command = string.format( 'awarn_warn "%s" "%s"', targetId, reasonArg )
-            caller:ConCommand( command )
-        else
-            RunConsoleCommand( "awarn_warn", targetId, reasonArg )
-        end
-
-        return
-    end
-
-    for _, target in pairs( targets ) do
-        local targetId = target:SteamID()
-        local stringTime = ULib.secondsToStringTime( duration * 60 )
-        local reasonArg = string.format( '%s (%s %s)', reason, commandName, stringTime )
-
-        if IsValid( caller ) then
-            local command = string.format( 'awarn_warn "%s" "%s"', targetId, reasonArg )
-            caller:ConCommand( command )
-        else
-            RunConsoleCommand( "awarn_warn", targetId, reasonArg )
-        end
+    for _, target in ipairs( targets ) do
+        reason = buildReason( target, reason, commandName, duration )
+        warn( caller, target, reason )
     end
 end )
