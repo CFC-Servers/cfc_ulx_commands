c71d2aabd3817fe13af7b3274d118889a341e694
Redox <mail@redox.to>
2023-07-03 19:44:36 +0200

    Add sragdoll and unsragdoll

---
+++ lua/ulx/modules/sh/cfc_silent_ragdoll.lua
@@ -0,0 +1,27 @@
+local function ragdoll( calling_ply, target_plys, should_unragdoll )
+	local affected_plys = {}
+	for i = 1, #target_plys do
+		local v = target_plys[i]
+
+		if not should_unragdoll then
+			if ulx.getExclusive( v, calling_ply ) then
+				ULib.tsayError( calling_ply, ulx.getExclusive( v, calling_ply ), true )
+			elseif not v:Alive() then
+				ULib.tsayError( calling_ply, v:Nick() .. " is dead and cannot be ragdolled!", true )
+			else
+				ulx.ragdollPlayer( v )
+				table.insert( affected_plys, v )
+			end
+		elseif v.ragdoll then -- Only if they're ragdolled...
+			ulx.unragdollPlayer( v )
+			table.insert( affected_plys, v )
+		end
+	end
+end
+
+local sragdoll = ulx.command( "Fun", "ulx sragdoll", ragdoll, "!sragdoll" )
+sragdoll:addParam{ type = ULib.cmds.PlayersArg }
+sragdoll:addParam{ type = ULib.cmds.BoolArg, invisible = true }
+sragdoll:defaultAccess( ULib.ACCESS_ADMIN )
+sragdoll:help( "sragdolls target(s)." )
+sragdoll:setOpposite( "ulx unsragdoll", { _, _, true }, "!unsragdoll" )
