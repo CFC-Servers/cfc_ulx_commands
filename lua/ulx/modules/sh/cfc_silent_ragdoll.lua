cb1da3c5cb125d27140dad4413ba06a28b7bb9c1
Redox <mail@redox.to>
2023-07-03 20:56:32 +0200

    Make sragdoll only self target

--- lua/ulx/modules/sh/cfc_silent_ragdoll.lua
+++ lua/ulx/modules/sh/cfc_silent_ragdoll.lua
@@ -1,27 +1,19 @@
-local function ragdoll( calling_ply, target_plys, should_unragdoll )
-	local affected_plys = {}
-	for i = 1, #target_plys do
-		local v = target_plys[i]
-
-		if not should_unragdoll then
-			if ulx.getExclusive( v, calling_ply ) then
-				ULib.tsayError( calling_ply, ulx.getExclusive( v, calling_ply ), true )
-			elseif not v:Alive() then
-				ULib.tsayError( calling_ply, v:Nick() .. " is dead and cannot be ragdolled!", true )
-			else
-				ulx.ragdollPlayer( v )
-				table.insert( affected_plys, v )
-			end
-		elseif v.ragdoll then -- Only if they're ragdolled...
-			ulx.unragdollPlayer( v )
-			table.insert( affected_plys, v )
-		end
-	end
+local function ragdoll( calling_ply, should_unragdoll )
+    if not should_unragdoll then
+        if ulx.getExclusive( calling_ply, calling_ply ) then
+            ULib.tsayError( calling_ply, ulx.getExclusive( calling_ply, calling_ply ), true )
+        elseif not calling_ply:Alive() then
+            ULib.tsayError( calling_ply, calling_ply:Nick() .. " is dead and cannot be ragdolled!", true )
+        else
+            ulx.ragdollPlayer( calling_ply )
+        end
+    elseif calling_ply.ragdoll then -- Only if they're ragdolled...
+        ulx.unragdollPlayer( calling_ply )
+    end
 end
 
 local sragdoll = ulx.command( "Fun", "ulx sragdoll", ragdoll, "!sragdoll" )
-sragdoll:addParam{ type = ULib.cmds.PlayersArg }
 sragdoll:addParam{ type = ULib.cmds.BoolArg, invisible = true }
 sragdoll:defaultAccess( ULib.ACCESS_SUPERADMIN )
 sragdoll:help( "sragdolls target(s)." )
-sragdoll:setOpposite( "ulx unsragdoll", { _, _, true }, "!unsragdoll" )
+sragdoll:setOpposite( "ulx unsragdoll", { _, true }, "!unsragdoll" )
