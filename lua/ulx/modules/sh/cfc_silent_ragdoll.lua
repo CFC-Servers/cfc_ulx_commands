ed73a6a986ec6d0a758741982742aefad7839f09
Redox <mail@redox.to>
2023-07-04 03:05:25 +0200

    Add sragdoll and unsragdoll (#117)
    * Add sragdoll and unsragdoll
    
    * Default to superadmin
    
    * Make sragdoll only self target

---
+++ lua/ulx/modules/sh/cfc_silent_ragdoll.lua
@@ -0,0 +1,19 @@
+local function ragdoll( calling_ply, should_unragdoll )
+    if not should_unragdoll then
+        if ulx.getExclusive( calling_ply, calling_ply ) then
+            ULib.tsayError( calling_ply, ulx.getExclusive( calling_ply, calling_ply ), true )
+        elseif not calling_ply:Alive() then
+            ULib.tsayError( calling_ply, calling_ply:Nick() .. " is dead and cannot be ragdolled!", true )
+        else
+            ulx.ragdollPlayer( calling_ply )
+        end
+    elseif calling_ply.ragdoll then -- Only if they're ragdolled...
+        ulx.unragdollPlayer( calling_ply )
+    end
+end
+
+local sragdoll = ulx.command( "Fun", "ulx sragdoll", ragdoll, "!sragdoll" )
+sragdoll:addParam{ type = ULib.cmds.BoolArg, invisible = true }
+sragdoll:defaultAccess( ULib.ACCESS_SUPERADMIN )
+sragdoll:help( "sragdolls target(s)." )
+sragdoll:setOpposite( "ulx unsragdoll", { _, true }, "!unsragdoll" )
