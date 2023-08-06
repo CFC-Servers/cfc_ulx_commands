a60c39cb49fc3fbc04f20432797a8a6fddf8abd6
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-08-06 17:16:55 -0500

    Block spawning and noclip manually

--- lua/ulx/modules/sh/timed_darkrp.lua
+++ lua/ulx/modules/sh/timed_darkrp.lua
@@ -5,19 +5,35 @@ local HELP = "Prevents the target(s) from spawning anything at all"
 
 if SERVER then
     local function enable( ply )
-        ply:DisallowNoclip( true )
-        ply:DisallowSpawning( true )
+        ply.isInULXDarkRP = true
 
         ply:StripWeapons()
         cleanup.CC_Cleanup( ply, "gmod_cleanup", {} )
     end
 
     local function disable( ply )
-        ply:DisallowNoclip( false )
-        ply:DisallowSpawning( false )
+        ply.isInULXDarkRP = nil
+    end
+
+    local function blockAction( ply )
+        if not ply or not ply:IsValid() then return end
+        if ply.isInULXDarkRP then return false end
     end
 
     TimedPunishments.Register( PUNISHMENT, enable, disable )
+
+    -- Block spawning and noclip while in darkrp mode.
+    -- Has to be done manually to prevent ulib's :DisallowSpawning() and etc from interfering, such as when a player unragdolls.
+    hook.Add( "PlayerNoClip", "ULibNoclipCheck", noclip, HOOK_HIGH )
+    hook.Add( "CanTool", "CFC_ULX_DarkRP_blockActioning", blockAction, HOOK_HIGH )
+    hook.Add( "PlayerSpawnObject", "CFC_ULX_DarkRP_blockActioning", blockAction )
+    hook.Add( "PlayerSpawnEffect", "CFC_ULX_DarkRP_blockActioning", blockAction )
+    hook.Add( "PlayerSpawnProp", "CFC_ULX_DarkRP_blockActioning", blockAction )
+    hook.Add( "PlayerSpawnNPC", "CFC_ULX_DarkRP_blockActioning", blockAction )
+    hook.Add( "PlayerSpawnVehicle", "CFC_ULX_DarkRP_blockActioning", blockAction )
+    hook.Add( "PlayerSpawnRagdoll", "CFC_ULX_DarkRP_blockActioning", blockAction )
+    hook.Add( "PlayerSpawnSENT", "CFC_ULX_DarkRP_blockActioning", blockAction )
+    hook.Add( "PlayerGiveSWEP", "CFC_ULX_DarkRP_blockActioning", blockAction )
 end
 
 local action = "forced ## into DarkRP Mode"
