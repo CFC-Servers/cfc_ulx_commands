b442a8b168b8cf55760ec15a65a98c7f27e53f86
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-08-28 12:30:00 -0600

    Use AdjustMouseSensitivity hook

--- lua/cfc_ulx_commands/curse/effects/aim_sensitivity.lua
+++ lua/cfc_ulx_commands/curse/effects/aim_sensitivity.lua
@@ -11,25 +11,16 @@ CFCUlxCurse.RegisterEffect( {
         if SERVER then return end
 
         local sensitivityMult = math.Rand( SENSITIVITY_MULT_MIN, SENSITIVITY_MULT_MAX )
-        local realAng
 
-        hook.Add( "CreateMove", HOOK_PREFIX .. "LBozo", function( cmd )
-            if not realAng then
-                realAng = cmd:GetViewAngles()
-            end
-
-            realAng.y = realAng.y - cmd:GetMouseX() * 0.022 * sensitivityMult
-            realAng.x = math.Clamp( realAng.x + cmd:GetMouseY() * 0.022 * sensitivityMult, -89, 89 )
-            realAng:Normalize()
-
-            cmd:SetViewAngles( realAng )
+        hook.Add( "AdjustMouseSensitivity", HOOK_PREFIX .. "LBozo", function()
+            return sensitivityMult
         end )
     end,
 
     onEnd = function()
         if SERVER then return end
 
-        hook.Remove( "CreateMove", HOOK_PREFIX .. "LBozo" )
+        hook.Remove( "AdjustMouseSensitivity", HOOK_PREFIX .. "LBozo" )
     end,
 
     minDuration = nil,
