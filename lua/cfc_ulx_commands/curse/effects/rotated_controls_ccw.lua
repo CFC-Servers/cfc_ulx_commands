06ef06a5cde28ede13b5e0acb5746e1bc259b012
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-08-27 18:30:22 -0600

    Add RotatedControls effects

---
+++ lua/cfc_ulx_commands/curse/effects/rotated_controls_ccw.lua
@@ -0,0 +1,39 @@
+local EFFECT_NAME = "RotatedControlsCCW"
+local HOOK_PREFIX = "CFC_ULXCommands_Curse_" .. EFFECT_NAME .. "_"
+
+
+CFCUlxCurse.RegisterEffect( {
+    name = EFFECT_NAME,
+
+    onStart = function()
+        if SERVER then return end
+
+        hook.Add( "CreateMove", HOOK_PREFIX .. "LBozo", function( cmd )
+            cmd:SetForwardMove( 0 )
+            cmd:SetSideMove( 0 )
+
+            if cmd:KeyDown( IN_FORWARD ) then
+                cmd:SetSideMove( -10000 )
+            elseif cmd:KeyDown( IN_BACK ) then
+                cmd:SetSideMove( 10000 )
+            end
+
+            if cmd:KeyDown( IN_MOVERIGHT ) then
+                cmd:SetForwardMove( 10000 )
+            elseif cmd:KeyDown( IN_MOVELEFT ) then
+                cmd:SetForwardMove( -10000 )
+            end
+        end )
+    end,
+
+    onEnd = function()
+        if SERVER then return end
+
+        hook.Remove( "CreateMove", HOOK_PREFIX .. "LBozo" )
+    end,
+
+    minDuration = nil,
+    maxDuration = nil,
+    onetimeDurationMult = nil,
+    excludeFromOnetime = true,
+} )
