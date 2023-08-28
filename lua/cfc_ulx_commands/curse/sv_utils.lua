2a0257c8839d907bd1ba38e70a00353ea25ff6e6
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-08-28 10:07:27 -0600

    Only loop through inflicted players

--- lua/cfc_ulx_commands/curse/sv_utils.lua
+++ lua/cfc_ulx_commands/curse/sv_utils.lua
@@ -1,5 +1,28 @@
+local inflictedPlayers = {} -- Players who either have an active one-time effect, or are timecursed (with or without an active effect).
+local inflictedPlayerLookup = {} -- Lookup table for inflictedPlayers.
+
 util.AddNetworkString( "CFC_ULXCommands_Curse_StartEffect" )
 util.AddNetworkString( "CFC_ULXCommands_Curse_EndEffect" )
+
+
+-- Track an inflicted player, so that effect expire/next timers can be updated.
+local function addInflictedPlayer( ply )
+    if inflictedPlayerLookup[ply] then return end
+
+    table.insert( inflictedPlayers, ply )
+    inflictedPlayerLookup[ply] = true
+end
+
+-- Stop tracking an inflicted player.
+local function removeInflictedPlayer( ply )
+    if not inflictedPlayerLookup[ply] then return end
+
+    table.RemoveByValue( inflictedPlayers, ply )
+    inflictedPlayerLookup[ply] = nil
+
+    ply.CFCUlxCurseNextEffectTime = nil
+    ply.CFCUlxCurseEffectExpireTime = nil
+end
 
 
 --[[
@@ -19,6 +42,7 @@ function CFCUlxCurse.ApplyCurseEffect( ply, effectData )
     ply.CFCUlxCurseEffect = effectData
     ply.CFCUlxCurseEffectExpireTime = RealTime() + duration
     effectData.onStart( ply )
+    addInflictedPlayer( ply )
 
     net.Start( "CFC_ULXCommands_Curse_StartEffect" )
     net.WriteString( effectData.name )
@@ -47,7 +71,7 @@ function CFCUlxCurse.StopCurseEffect( ply )
 
         ply.CFCUlxCurseNextEffectTime = RealTime() + gap
     else
-        ply.CFCUlxCurseNextEffectTime = nil
+        removeInflictedPlayer( ply )
     end
 end
 
@@ -57,11 +81,12 @@ end
 hook.Add( "PlayerDisconnected", "CFC_ULXCommands_Curse_StopEffectOnLeave", function( ply )
     if not IsValid( ply ) then return end
 
+    removeInflictedPlayer( ply )
+
     local prevEffect = CFCUlxCurse.GetCurrentEffect( ply )
     if not prevEffect then return end
 
     ply.CFCUlxCurseEffect = nil
-    ply.CFCUlxCurseEffectExpireTime = nil
     prevEffect.onEnd( ply )
 end )
 
@@ -69,7 +94,7 @@ end )
 timer.Create( "CFC_ULXCommands_Curse_StartAndStopEffects", 5, 0, function()
     local now = RealTime()
 
-    for _, ply in ipairs( player.GetAll() ) do
+    for _, ply in ipairs( inflictedPlayers ) do
         local effectExpireTime = ply.CFCUlxCurseEffectExpireTime
         local nextEffectTime = ply.CFCUlxCurseNextEffectTime
 
