46bf2ae2a35ba8ce1db42eba2c119e954fef805e
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-08-28 20:20:34 -0600

    Use ScreenScaleH()

--- lua/cfc_ulx_commands/curse/effects/health_obfuscate.lua
+++ lua/cfc_ulx_commands/curse/effects/health_obfuscate.lua
@@ -9,29 +9,25 @@ local HOOK_PREFIX = "CFC_ULXCommands_Curse_" .. EFFECT_NAME .. "_"
 --]]
 
 
-local SCALE
 local COLOR_BACKGROUND = Color( 0, 0, 0, 76 )
 local HEALTH_LABEL_FONT = HOOK_PREFIX .. "HealthLabel"
 local HEALTH_NUMBER_FONT = HOOK_PREFIX .. "HealthNumbers"
 
 if CLIENT then
-    -- Values and scaling method taken from experimentation, HudLayout.res, and https://developer.valvesoftware.com/wiki/VGUI_Documentation
-    SCALE = ScrH() / 480
-
     --[[
         - HL2 text rendering auto-scales fonts to the screen resolution, while glua rendering does not.
         - As such, we have to create our own fonts with the correct scaling factor.
     --]]
     surface.CreateFont( HEALTH_LABEL_FONT, {
         font = "Verdana",
-        size = 9 * SCALE,
+        size = ScreenScaleH( 9 ),
         weight = 1000,
         antialias = true,
     } )
 
     surface.CreateFont( HEALTH_NUMBER_FONT, {
         font = "HalfLife2",
-        size = 32 * SCALE,
+        size = ScreenScaleH( 32 ),
         weight = 0,
         antialias = true,
         additive = true,
@@ -53,20 +49,21 @@ CFCUlxCurse.RegisterEffect( {
             if not LocalPlayer():Alive() then return end
 
             local fakeHealth = math.random( 1, LocalPlayer():GetMaxHealth() )
+            local scale = ScreenScaleH( 1 )
 
             -- Background
-            draw.RoundedBox( 4 * SCALE, 16 * SCALE, 432 * SCALE, 102 * SCALE, 36 * SCALE, COLOR_BACKGROUND )
+            draw.RoundedBox( 4 * scale, 16 * scale, 432 * scale, 102 * scale, 36 * scale, COLOR_BACKGROUND )
 
             -- "HEALTH" Text
             surface.SetFont( HEALTH_LABEL_FONT )
             surface.SetTextColor( 255, 236, 12, 255 )
-            surface.SetTextPos( ( 16 + 8 ) * SCALE, ( 432 + 20 ) * SCALE )
+            surface.SetTextPos( ( 16 + 8 ) * scale, ( 432 + 20 ) * scale )
             surface.DrawText( "HEALTH", false )
 
             -- Health Number
             surface.SetFont( HEALTH_NUMBER_FONT )
             surface.SetTextColor( 255, 236, 12, 255 )
-            surface.SetTextPos( ( 16 + 50 ) * SCALE, ( 432 + 2 ) * SCALE )
+            surface.SetTextPos( ( 16 + 50 ) * scale, ( 432 + 2 ) * scale )
             surface.DrawText( tostring( fakeHealth ), true )
         end )
     end,
