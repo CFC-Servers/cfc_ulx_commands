317c04ec11316e0369e9dc6a9f0c9e1dd5d3d7ac
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-09-04 01:22:23 -0600

    Fix concmd args

--- lua/cfc_ulx_commands/curse/effects/smeary.lua
+++ lua/cfc_ulx_commands/curse/effects/smeary.lua
@@ -7,17 +7,17 @@ CFCUlxCurse.RegisterEffect( {
     onStart = function()
         if SERVER then return end
 
-        RunConsoleCommand( "pp_fb 100000000000000000000000000000000000000" )
-        RunConsoleCommand( "pp_fb_shutter -0.0001" )
-        RunConsoleCommand( "pp_fb_frames 32" )
+        RunConsoleCommand( "pp_fb", "100000000000000000000000000000000000000" )
+        RunConsoleCommand( "pp_fb_shutter", "-0.0001" )
+        RunConsoleCommand( "pp_fb_frames", "32" )
     end,
 
     onEnd = function()
         if SERVER then return end
 
-        RunConsoleCommand( "pp_fb 0" )
-        RunConsoleCommand( "pp_fb_shutter 0.5" )
-        RunConsoleCommand( "pp_fb_frames 16" )
+        RunConsoleCommand( "pp_fb", "0" )
+        RunConsoleCommand( "pp_fb_shutter", "0.5" )
+        RunConsoleCommand( "pp_fb_frames", "16" )
     end,
 
     minDuration = nil,
