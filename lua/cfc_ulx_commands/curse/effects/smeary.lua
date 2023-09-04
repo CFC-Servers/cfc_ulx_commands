38d9511226e77d38dc9b2ac6283fd824ece83f27
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-09-04 01:02:32 -0600

    Add Smeary effect

---
+++ lua/cfc_ulx_commands/curse/effects/smeary.lua
@@ -0,0 +1,27 @@
+local EFFECT_NAME = "Smeary"
+
+
+CFCUlxCurse.RegisterEffect( {
+    name = EFFECT_NAME,
+
+    onStart = function()
+        if SERVER then return end
+
+        RunConsoleCommand( "pp_fb 100000000000000000000000000000000000000" )
+        RunConsoleCommand( "pp_fb_shutter -0.0001" )
+        RunConsoleCommand( "pp_fb_frames 32" )
+    end,
+
+    onEnd = function()
+        if SERVER then return end
+
+        RunConsoleCommand( "pp_fb 0" )
+        RunConsoleCommand( "pp_fb_shutter 0.5" )
+        RunConsoleCommand( "pp_fb_frames 16" )
+    end,
+
+    minDuration = nil,
+    maxDuration = nil,
+    onetimeDurationMult = nil,
+    excludeFromOnetime = nil,
+} )
