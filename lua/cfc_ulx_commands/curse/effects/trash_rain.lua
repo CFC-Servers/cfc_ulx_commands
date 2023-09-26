0ab3d4f68e8f93d5f8c701bed2c6080384c7bb0c
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-09-26 16:02:29 -0600

    Create TrashRain effect

---
+++ lua/cfc_ulx_commands/curse/effects/trash_rain.lua
@@ -0,0 +1,167 @@
+local EFFECT_NAME = "TrashRain"
+local TRASH_SPAWN_HEIGHT_MIN = 500
+local TRASH_SPAWN_HEIGHT_MAX = 650
+local TRASH_SPAWN_RADIUS_MIN = 0
+local TRASH_SPAWN_RADIUS_MAX = 150
+local TRASH_SPAWN_INTERVAL = 0.05
+local TRASH_SPAWN_CHANCE = 0.25
+local TRASH_MAX = 50
+local TRASH_VELOCITY_SPEED_MIN = 2000
+local TRASH_VELOCITY_SPEED_MAX = 2500
+local TRASH_VELOCITY_SPREAD = 10 -- In degrees 0-89.9
+local TRASH_LIFETIME = 10
+local TRASH_FADE_DURATION = 1
+local TRASH_UPDATE_INTERVAL = 0.05
+local TRASH_MODELS = {
+    "models/props_interiors/Radiator01a.mdl",
+    "models/props_c17/oildrum001.mdl",
+    "models/props_c17/FurnitureSink001a.mdl",
+    "models/props_junk/garbage_milkcarton001a.mdl",
+    "models/props_lab/harddrive02.mdl",
+    "models/props_interiors/pot01a.mdl",
+    "models/props_combine/breenglobe.mdl",
+    "models/props_wasteland/controlroom_chair001a.mdl",
+    "models/props_junk/TrafficCone001a.mdl",
+    "models/props_wasteland/prison_lamp001c.mdl",
+    "models/props_junk/MetalBucket02a.mdl",
+    "models/props_c17/chair02a.mdl",
+}
+local HOOK_PREFIX = "CFC_ULXCommands_Curse_" .. EFFECT_NAME .. "_"
+
+
+local TRASH_MODEL_COUNT = #TRASH_MODELS
+
+local trashEnts = {}
+local fadingTrashEnts = {}
+local spawnTrash
+local startFadingTrash
+local trySpawnTrash
+local updateTrash
+
+
+CFCUlxCurse.RegisterEffect( {
+    name = EFFECT_NAME,
+
+    onStart = function()
+        if SERVER then return end
+
+        timer.Create( HOOK_PREFIX .. "SpawnTrash", TRASH_SPAWN_INTERVAL, 0, trySpawnTrash )
+        timer.Create( HOOK_PREFIX .. "UpdateTrash", TRASH_UPDATE_INTERVAL, 0, updateTrash )
+    end,
+
+    onEnd = function()
+        if SERVER then return end
+
+        timer.Remove( HOOK_PREFIX .. "SpawnTrash" )
+        timer.Remove( HOOK_PREFIX .. "UpdateTrash" )
+
+        for i = #trashEnts, 1, -1 do
+            local ent = trashEnts[i]
+            trashEnts[i] = nil
+
+            if IsValid( ent ) then
+                ent:Remove()
+            end
+        end
+
+        for i = #fadingTrashEnts, 1, -1 do
+            local ent = fadingTrashEnts[i]
+            fadingTrashEnts[i] = nil
+
+            if IsValid( ent ) then
+                ent:Remove()
+            end
+        end
+    end,
+
+    minDuration = nil,
+    maxDuration = nil,
+    onetimeDurationMult = nil,
+    excludeFromOnetime = nil,
+} )
+
+
+if SERVER then
+    for _, model in pairs( TRASH_MODELS ) do
+        util.PrecacheModel( model )
+    end
+
+    return
+end
+
+
+spawnTrash = function()
+    local pos = LocalPlayer():GetPos()
+    local ang = Angle( math.Rand( -180, 180 ), math.Rand( -180, 180 ), math.Rand( -180, 180 ) )
+    local model = TRASH_MODELS[math.random( TRASH_MODEL_COUNT )]
+
+    local height = math.Rand( TRASH_SPAWN_HEIGHT_MIN, TRASH_SPAWN_HEIGHT_MAX )
+    local radius = math.Rand( TRASH_SPAWN_RADIUS_MIN, TRASH_SPAWN_RADIUS_MAX )
+    local theta = math.Rand( 0, 360 )
+    local vel = Angle( math.Rand( 89.9 - TRASH_VELOCITY_SPREAD, 89.9 ), math.Rand( -180, 180 ), 0 ):Forward()
+    vel = vel * math.Rand( TRASH_VELOCITY_SPEED_MIN, TRASH_VELOCITY_SPEED_MAX )
+
+    pos = pos + Vector( math.cos( theta ) * radius, math.sin( theta ) * radius, height )
+
+    local ent = ents.CreateClientProp( model )
+    ent:SetPos( pos )
+    ent:SetAngles( ang )
+    ent:SetRenderMode( RENDERMODE_TRANSCOLOR )
+    ent:Spawn()
+    ent:GetPhysicsObject():SetVelocity( vel )
+
+    ent.CFCUlxCurseFadeStartTime = CurTime() + TRASH_LIFETIME
+    table.insert( trashEnts, ent )
+
+    if #trashEnts > TRASH_MAX then
+        startFadingTrash( trashEnts[1] )
+    end
+end
+
+startFadingTrash = function( ent )
+    if ent.CFCUlxCurseFading then return end
+
+    ent.CFCUlxCurseFading = true
+    ent.CFCUlxCurseFadeStartTime = CurTime()
+
+    table.RemoveByValue( trashEnts, ent )
+    table.insert( fadingTrashEnts, ent )
+end
+
+trySpawnTrash = function()
+    if math.Rand( 0, 1 ) > TRASH_SPAWN_CHANCE then return end
+
+    spawnTrash()
+end
+
+updateTrash = function()
+    local now = CurTime()
+
+    for i = #trashEnts, 1, -1 do
+        local ent = trashEnts[i]
+
+        if not ent:IsValid() then
+            table.remove( trashEnts, i )
+        elseif now > ent.CFCUlxCurseFadeStartTime then
+            startFadingTrash( ent )
+        end
+    end
+
+    for i = #fadingTrashEnts, 1, -1 do
+        local ent = fadingTrashEnts[i]
+
+        if not ent:IsValid() then
+            table.remove( fadingTrashEnts, i )
+        else
+            local elapsed = now - ent.CFCUlxCurseFadeStartTime
+
+            if elapsed >= TRASH_FADE_DURATION then
+                table.remove( fadingTrashEnts, i )
+                ent:Remove()
+            else
+                local alpha = 255 * ( 1 - elapsed / TRASH_FADE_DURATION )
+                ent:SetColor( Color( 255, 255, 255, alpha ) )
+            end
+        end
+    end
+end
