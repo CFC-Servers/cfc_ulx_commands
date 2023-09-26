ba7d6827fa8cb3f08b62a7670ec8e8f4f18c446b
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-09-26 10:20:29 -0600

    Create Crab effect

---
+++ lua/cfc_ulx_commands/curse/effects/crab.lua
@@ -0,0 +1,26 @@
+local EFFECT_NAME = "Crab"
+local HOOK_PREFIX = "CFC_ULXCommands_Curse_" .. EFFECT_NAME .. "_"
+
+
+CFCUlxCurse.RegisterEffect( {
+    name = EFFECT_NAME,
+
+    onStart = function()
+        if SERVER then return end
+
+        hook.Add( "CreateMove", HOOK_PREFIX .. "LBozo", function( cmd )
+            cmd:SetForwardMove( 0 )
+        end )
+    end,
+
+    onEnd = function()
+        if SERVER then return end
+
+        hook.Remove( "CreateMove", HOOK_PREFIX .. "LBozo" )
+    end,
+
+    minDuration = nil,
+    maxDuration = nil,
+    onetimeDurationMult = nil,
+    excludeFromOnetime = nil,
+} )
