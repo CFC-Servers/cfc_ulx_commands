b667d98f5bfb49603124771062ff4c2ebca2f9d2
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-11-05 17:06:46 -0700

    Don't block the main menu

--- lua/cfc_ulx_commands/curse/effects/no_hud.lua
+++ lua/cfc_ulx_commands/curse/effects/no_hud.lua
@@ -1,4 +1,7 @@
 local EFFECT_NAME = "NoHud"
+local ALLOWED_HUD_ELEMENTS = {
+    CHudMenu = true,
+}
 
 
 CFCUlxCurse.RegisterEffect( {
@@ -7,7 +10,9 @@ CFCUlxCurse.RegisterEffect( {
     onStart = function( cursedPly )
         if SERVER then return end
 
-        CFCUlxCurse.AddEffectHook( cursedPly, EFFECT_NAME, "HUDShouldDraw", "NoHudPls", function()
+        CFCUlxCurse.AddEffectHook( cursedPly, EFFECT_NAME, "HUDShouldDraw", "NoHudPls", function( elementName )
+            if ALLOWED_HUD_ELEMENTS[elementName] then return end
+
             return false
         end )
     end,
