0a12d0c3b6864748361285905891750629042601
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-06-30 19:11:59 -0500

    Show players and NPCs

--- lua/cfc_ulx_commands/curse/effects/lidar.lua
+++ lua/cfc_ulx_commands/curse/effects/lidar.lua
@@ -1,14 +1,20 @@
 local EFFECT_NAME = "Lidar"
-local VERTS_PER_MESH = 500 * 3
 local MESH_LIMIT = 8000
+local VERTS_PER_MESH = 500 * 3
+local VERTS_PER_EXPIRABLE_MESH = 50 * 3
+local MESH_EXPIRE_TIME = 2
 local SCAN_INTERVAL = 0.01
 local DOTS_PER_SCAN = 75
 local DOT_SPREAD = 45
 local DOT_SIZE = 3
+local DOT_HITBOX_SIZE = 2
 local SKY_COLOR = Color( 130, 230, 230, 255 )
 local WATER_COLOR = Color( 50, 100, 225, 255 )
 local SLIME_COLOR = Color( 140, 120, 15, 255 )
+local PLAYER_COLOR = Color( 255, 0, 0, 255 )
+local NPC_COLOR = Color( 200, 0, 255, 255 )
 local TRACE_MASK = MASK_SOLID + CONTENTS_WATER + CONTENTS_SLIME
+local TRACE_MASK_HITBOXES = MASK_SHOT + CONTENTS_WATER + CONTENTS_SLIME
 
 
 local DOT_SPREAD_HALF = DOT_SPREAD / 2
@@ -16,11 +22,18 @@ local ROTATE_THIRD = 360 / 3
 local VECTOR_ZERO = Vector( 0, 0, 0 )
 
 local meshes = {}
+local expirableMeshes = {}
 local curMesh = nil
+local curMeshChanged = false
 local curMeshData = {}
+local curExpirableMesh = nil
+local curExpirableMeshChanged = false
+local curExpirableMeshData = {}
 local meshCount = 0
+local expirableMeshCount = 0
 local meshHead = 1
 local curVertCount = 0
+local curExpirableVertCount = 0
 local placingDots = false
 local showDotHint = false
 local lidarMat = nil
@@ -58,8 +71,34 @@ local function addDot( startPos, dir, filter )
     if hitNormal == VECTOR_ZERO then return end
 
     local hitPos = tr.HitPos
+    local hitEnt = tr.Entity
     local contents = tr.Contents
     local color
+    local putOnExpirableMesh = false
+    local size = DOT_SIZE
+
+    hitEnt = IsValid( hitEnt ) and hitEnt or nil
+
+    if hitEnt and ( hitEnt:IsPlayer() or hitEnt:IsNPC() ) then
+        local tr2 = utilTraceLine( {
+            start = startPos,
+            endpos = endPos,
+            filter = filter,
+            mask = TRACE_MASK_HITBOXES,
+        } )
+
+        local newEnt = tr2.Entity
+
+        if tr2.Hit then
+            if newEnt == hitEnt then
+                size = DOT_HITBOX_SIZE
+            end
+
+            hitPos = tr2.HitPos
+            hitNormal = tr2.HitNormal
+            hitEnt = newEnt
+        end
+    end
 
     if tr.HitSky then
         color = SKY_COLOR
@@ -67,6 +106,11 @@ local function addDot( startPos, dir, filter )
         color = WATER_COLOR
     elseif bitBand( contents, CONTENTS_SLIME ) ~= 0 then
         color = SLIME_COLOR
+    elseif hitEnt and hitEnt:IsPlayer() then
+        color = PLAYER_COLOR
+        putOnExpirableMesh = true
+    elseif hitEnt and hitEnt:IsNPC() then
+        color = NPC_COLOR
     else
         color = renderGetSurfaceColor( hitPos - dir * 5, hitPos + dir * 5 )
         color = Color( color.x * 255, color.y * 255, color.z * 255, 255 )
@@ -80,26 +124,40 @@ local function addDot( startPos, dir, filter )
 
         local offset = offsetAng:Forward()
 
-        if DOT_SIZE ~= 1 then
-            offset = offset * DOT_SIZE
+        if size ~= 1 then
+            offset = offset * size
         end
 
-        curVertCount = curVertCount + 1
-        curMeshData[curVertCount] = {
-            pos = hitPos + offset,
-            color = color,
-            normal = hitNormal,
-        }
+        if putOnExpirableMesh then
+            curExpirableMeshChanged = true
+            curExpirableVertCount = curExpirableVertCount + 1
+            curExpirableMeshData[curExpirableVertCount] = {
+                pos = hitPos + offset,
+                color = color,
+                normal = hitNormal,
+            }
+        else
+            curMeshChanged = true
+            curVertCount = curVertCount + 1
+            curMeshData[curVertCount] = {
+                pos = hitPos + offset,
+                color = color,
+                normal = hitNormal,
+            }
+        end
     end
 end
 
 local function updateCurMesh()
+    if not curMeshChanged then return end
+
     if curMesh then
         curMesh:Destroy()
     end
 
     curMesh = Mesh()
     curMesh:BuildFromTriangles( curMeshData )
+    curMeshChanged = false
 
     -- Split off into a new mesh
     if curVertCount <= VERTS_PER_MESH then return end
@@ -131,10 +189,35 @@ local function updateCurMesh()
     end
 end
 
+local function updateCurExpirableMesh()
+    if not curExpirableMeshChanged then return end
+
+    if curExpirableMesh then
+        curExpirableMesh:Destroy()
+    end
+
+    curExpirableMesh = Mesh()
+    curExpirableMesh:BuildFromTriangles( curExpirableMeshData )
+    curExpirableMeshChanged = false
+
+    -- Split off into a new mesh
+    if curExpirableVertCount <= VERTS_PER_EXPIRABLE_MESH then return end
+
+    expirableMeshCount = expirableMeshCount + 1
+    expirableMeshes[expirableMeshCount] = {
+        mesh = curExpirableMesh,
+        expireTime = CurTime() + MESH_EXPIRE_TIME,
+    }
+
+    curExpirableMesh = nil
+    curExpirableMeshData = {}
+    curExpirableVertCount = 0
+end
+
 local function addDots( eyePos, eyeAng )
     local eyeRight = eyeAng:Right()
     local eyeUp = eyeAng:Up()
-    local filter = playerGetAll()
+    local filter = LocalPlayer()
 
     for _ = 1, DOTS_PER_SCAN do
         local dir = spreadDirFast( eyeAng, eyeRight, eyeUp )
@@ -143,9 +226,12 @@ local function addDots( eyePos, eyeAng )
     end
 
     updateCurMesh()
+    updateCurExpirableMesh()
 end
 
 local function drawMeshes()
+    local now = CurTime()
+
     surfaceSetDrawColor( 255, 255, 255, 255 )
     renderSetMaterial( lidarMat )
 
@@ -153,8 +239,25 @@ local function drawMeshes()
         meshes[i]:Draw()
     end
 
+    for i = expirableMeshCount, 1, -1 do
+        local meshInfo = expirableMeshes[i]
+        local meshObj = meshInfo.mesh
+
+        if meshInfo.expireTime < now then
+            meshObj:Destroy()
+            table.remove( expirableMeshes, i )
+            expirableMeshCount = expirableMeshCount - 1
+        else
+            meshObj:Draw()
+        end
+    end
+
     if curMesh then
         curMesh:Draw()
+    end
+
+    if curExpirableMesh then
+        curExpirableMesh:Draw()
     end
 end
 
@@ -177,7 +280,12 @@ CFCUlxCurse.RegisterEffect( {
         showDotHint = true
         curMesh = nil
         curMeshData = {}
+        curVertCount = 0
+        curExpirableMesh = nil
+        curExpirableMeshData = {}
+        curExpirableVertCount = 0
         meshCount = 0
+        expirableMeshCount = 0
         meshHead = 1
 
         CFCUlxCurse.AddEffectHook( cursedPly, EFFECT_NAME, "RenderScene", "CustomRender", function()
@@ -238,9 +346,21 @@ CFCUlxCurse.RegisterEffect( {
             meshes[i] = nil
         end
 
+        for i = 1, expirableMeshCount do
+            local meshInfo = expirableMeshes[i]
+            local meshObj = meshInfo.mesh
+
+            meshObj:Destroy()
+        end
+
         if curMesh then
             curMesh:Destroy()
             curMesh = nil
+        end
+
+        if curExpirableMesh then
+            curExpirableMesh:Destroy()
+            curExpirableMesh = nil
         end
     end,
 
