a981f5846b2b84fc634127341c8fba6ef96ec888
ProfessorBoxtrot <ProfessorBoxtrot@gmail.com>
2024-11-06 18:35:38 -0800

    Add the DoSomeHarm curse

---
+++ lua/cfc_ulx_commands/curse/effects/do_some_harm.lua
@@ -0,0 +1,29 @@
+local EFFECT_NAME = "DoSomeHarm"
+local DAMAGE_BLOCK_CHANCE = 0.75
+
+CFCUlxCurse.RegisterEffect( {
+    name = EFFECT_NAME,
+
+    onStart = function( cursedPly )
+        if CLIENT then return end
+		
+        CFCUlxCurse.AddEffectHook( cursedPly, EFFECT_NAME, "EntityTakeDamage", "BlockDamage", function( ply, dmgInfo )
+			if dmgInfo:GetAttacker() ~= cursedPly or math.random() > DAMAGE_BLOCK_CHANCE then return end
+			return true
+		end)
+    end,
+
+    onEnd = function()
+        -- Do nothing.
+    end,
+
+    minDuration = nil,
+    maxDuration = nil,
+    onetimeDurationMult = nil,
+    excludeFromOnetime = true,
+    incompatibileEffects = {},
+    groups = {
+		"DisableDamage"
+	},
+    incompatibleGroups = {},
+} )
